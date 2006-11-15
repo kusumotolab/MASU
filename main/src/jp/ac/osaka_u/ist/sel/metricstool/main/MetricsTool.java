fa83db50f34ca29b36fea748b54bbe88d03c9283
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2006-11-15 08:34:04 +0000

    Package name was changed. metricstool.main.data.file -> metricstool.main.data.target
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@15 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/MetricsTool.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/MetricsTool.java
@@ -2,8 +2,9 @@ package jp.ac.osaka_u.ist.sel.metricstool.main;
 
 
 import java.io.FileInputStream;
-import jp.ac.osaka_u.ist.sel.metricstool.main.data.file.TargetFile;
-import jp.ac.osaka_u.ist.sel.metricstool.main.data.file.TargetFileData;
+
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TargetFile;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TargetFileData;
 import jp.ac.osaka_u.ist.sel.metricstool.main.parse.Java15Lexer;
 import jp.ac.osaka_u.ist.sel.metricstool.main.parse.Java15Parser;
 
