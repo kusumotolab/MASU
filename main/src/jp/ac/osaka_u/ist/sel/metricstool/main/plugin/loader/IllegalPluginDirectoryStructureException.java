58771630c33f0cb35ec32a5149b4a32ad7c3ab43
kou-tngt <kou-tngt@5bdb5694-003a-4164-b60f-8fcef0365520>
2006-11-17 19:13:39 +0000

    Modify exception name.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@32 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/plugin/loader/IllegalPluginDirectoryStructureException.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/plugin/loader/IllegalPluginDirectoryStructureException.java
@@ -6,21 +6,21 @@ package jp.ac.osaka_u.ist.sel.metricstool.main.plugin.loader;
  * この例外は，プラグインのディレクトリ構成がプラグインの規則に従っていない場合に投げられる．
  * 具体的には，plugin.xmlがプラグインのディレクトリ直下に存在しない場合などである．
  */
-public class IlleagalPluginDirectoryStructureException extends PluginLoadException {
+public class IllegalPluginDirectoryStructureException extends PluginLoadException {
 
-    public IlleagalPluginDirectoryStructureException() {
+    public IllegalPluginDirectoryStructureException() {
         super();
     }
 
-    public IlleagalPluginDirectoryStructureException(String message, Throwable cause) {
+    public IllegalPluginDirectoryStructureException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public IlleagalPluginDirectoryStructureException(String message) {
+    public IllegalPluginDirectoryStructureException(String message) {
         super(message);
     }
 
-    public IlleagalPluginDirectoryStructureException(Throwable cause) {
+    public IllegalPluginDirectoryStructureException(Throwable cause) {
         super(cause);
     }
 
