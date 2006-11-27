3c52b10f3e917134d447bb590fd9fbf842e90f3c
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2006-11-27 04:18:02 +0000

    FieldInfoManager manages FieldInfo objects.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@107 5bdb5694-003a-4164-b60f-8fcef0365520

---
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/FieldInfoManager.java
@@ -0,0 +1,76 @@
+package jp.ac.osaka_u.ist.sel.metricstool.main.data.target;
+
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import jp.ac.osaka_u.ist.sel.metricstool.main.security.MetricsToolSecurityManager;
+
+
+/**
+ * フィールド情報を管理するクラス． FieldInfo を要素として持つ．
+ * 
+ * @author y-higo
+ * 
+ */
+public final class FieldInfoManager implements Iterable<FieldInfo> {
+
+    /**
+     * フィールド情報を管理しているインスタンスを返す． シングルトンパターンを持ちている．
+     * 
+     * @return フィールド情報を管理しているインスタンス
+     */
+    public static FieldInfoManager getInstance() {
+        if (FIELD_INFO_DATA == null) {
+            FIELD_INFO_DATA = new FieldInfoManager();
+        }
+        return FIELD_INFO_DATA;
+    }
+
+    /**
+     * フィールド情報を追加する
+     * 
+     * @param fieldInfo 追加するフィールド情報
+     */
+    public void add(final FieldInfo fieldInfo) {
+
+        MetricsToolSecurityManager.getInstance().checkAccess();
+        if (null == fieldInfo) {
+            throw new NullPointerException();
+        }
+
+        this.fieldInfos.add(fieldInfo);
+    }
+
+    /**
+     * フィールド情報の Iterator を返す．この Iterator は unmodifiable であり，変更操作を行うことはできない．
+     */
+    public Iterator<FieldInfo> iterator() {
+        Set<FieldInfo> unmodifiableFieldInfos = Collections.unmodifiableSet(this.fieldInfos);
+        return unmodifiableFieldInfos.iterator();
+    }
+
+    /**
+     * 
+     * コンストラクタ． シングルトンパターンで実装しているために private がついている．
+     */
+    private FieldInfoManager() {
+        MetricsToolSecurityManager.getInstance().checkAccess();
+        this.fieldInfos = new TreeSet<FieldInfo>();
+    }
+
+    /**
+     * 
+     * シングルトンパターンを実装するための変数．
+     */
+    private static FieldInfoManager FIELD_INFO_DATA = null;
+
+    /**
+     * 
+     * フィールド情報 (FieldInfo) を格納する変数．
+     */
+    private final SortedSet<FieldInfo> fieldInfos;
+}
