9b2444c96446be1c7412294aeb2f818e07d00813
kou-tngt <kou-tngt@5bdb5694-003a-4164-b60f-8fcef0365520>
2006-11-28 02:05:52 +0000

    sample_plugin was updated.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@113 5bdb5694-003a-4164-b60f-8fcef0365520

--- sample_plugin/src/jp/ac/osaka_u/ist/sel/metricstool/sample/SamplePlugin.java
+++ sample_plugin/src/jp/ac/osaka_u/ist/sel/metricstool/sample/SamplePlugin.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import jp.ac.osaka_u.ist.sel.metricstool.main.plugin.AbstractPlugin;
 import jp.ac.osaka_u.ist.sel.metricstool.main.util.LANGUAGE;
-import jp.ac.osaka_u.ist.sel.metricstool.main.util.METRICS_TYPE;
+import jp.ac.osaka_u.ist.sel.metricstool.main.util.METRIC_TYPE;
 import library.ClassPathTest;
 
 
@@ -19,10 +19,10 @@ public class SamplePlugin extends AbstractPlugin {
     @Override
     protected LANGUAGE[] getMeasurableLanguages() {
         //Enumを全部取得
-        LANGUAGE[] languages = LANGUAGE.values();
-        List<LANGUAGE> resultList = new ArrayList<LANGUAGE>();
+        final LANGUAGE[] languages = LANGUAGE.values();
+        final List<LANGUAGE> resultList = new ArrayList<LANGUAGE>();
         
-        for(LANGUAGE language : languages){
+        for(final LANGUAGE language : languages){
             if (language.isObjectOrientedLanguage()){
                 //Enumの言語でオブジェクト指向言語なものだけをリストに詰める
                 resultList.add(language);
@@ -30,19 +30,19 @@ public class SamplePlugin extends AbstractPlugin {
         }
         
         //戻り値用の配列を作って返す．
-        LANGUAGE[] resultArray = new LANGUAGE[resultList.size()];
+        final LANGUAGE[] resultArray = new LANGUAGE[resultList.size()];
         return resultList.toArray(resultArray);
     }
 
 
     @Override
-    protected String getMetricsName() {
+    protected String getMetricName() {
         return "sample";
     }
 
     @Override
-    protected METRICS_TYPE getMetricsType() {
-        return METRICS_TYPE.FILE_METRICS;
+    protected METRIC_TYPE getMetricType() {
+        return METRIC_TYPE.FILE_METRIC;
     }
 
     @Override
@@ -55,7 +55,7 @@ public class SamplePlugin extends AbstractPlugin {
         System.out.println("======================");
         System.out.println("Instanciate ClassPathTest ...");
         
-        ClassPathTest test = new ClassPathTest();
+        final ClassPathTest test = new ClassPathTest();
         System.out.println("success!");
         System.out.println();
         System.out.println(test);
