2c1f2865ecc0118f2a4427982f949e5270c435ab
kou-tngt <kou-tngt@5bdb5694-003a-4164-b60f-8fcef0365520>
2006-11-30 08:42:39 +0000

    The default message printers out and err were added to AbstractPlugin. The default progress reporter and reporting method were added to AbstractPlugin. The method AbstractPlugin#executeWrapper() which wrap the invokation of #execute().
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@152 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/io/ProgressReporter.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/io/ProgressReporter.java
@@ -8,6 +8,11 @@ package jp.ac.osaka_u.ist.sel.metricstool.main.io;
  */
 public interface ProgressReporter {
     /**
+     * 進捗情報送信の終了を報告するメソッド
+     */
+    public void reportProgressEnd();
+
+    /**
      * 進捗情報を報告するメソッド
      * @param percentage 進捗値（%）
      * @throws IllegalArgumentException percentageが0-100の間に入ってない場合
