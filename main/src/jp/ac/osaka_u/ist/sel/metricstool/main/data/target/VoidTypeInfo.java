b9c0f3ee0fee4a713c775dfa0a023f4c9fdb7da7
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2006-12-22 07:12:28 +0000

    private  constructor was added.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@201 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/VoidTypeInfo.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/VoidTypeInfo.java
@@ -1,5 +1,6 @@
 package jp.ac.osaka_u.ist.sel.metricstool.main.data.target;
 
+
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.UnresolvedTypeInfo;
 
 
@@ -16,36 +17,48 @@ public final class VoidTypeInfo implements TypeInfo, UnresolvedTypeInfo {
      * 
      * @return このクラスの単一オブジェクト
      */
-    public static VoidTypeInfo getInstance(){
+    public static VoidTypeInfo getInstance() {
         return SINGLETON;
     }
-    
+
     /**
      * void 型の名前を返す．
      */
     public String getTypeName() {
-        return VOID_STRING;
+        return this.name;
     }
 
     /**
      * 等しいかどうかのチェックを行う
      */
-    public boolean equals(final TypeInfo typeInfo){
-        
-        if (null == typeInfo){
+    public boolean equals(final TypeInfo typeInfo) {
+
+        if (null == typeInfo) {
             throw new NullPointerException();
         }
-        
+
         return typeInfo instanceof VoidTypeInfo;
     }
-   
+
     /**
      * void 型の型名を表す定数
      */
     public static final String VOID_STRING = new String("void");
 
     /**
+     * 引数なしコンストラクタ
+     */
+    private VoidTypeInfo() {
+        this.name = VOID_STRING;
+    }
+
+    /**
      * このクラスの単一オブジェクトを保存するための定数
      */
     private static final VoidTypeInfo SINGLETON = new VoidTypeInfo();
+
+    /**
+     * この型の名前を保存するための変数
+     */
+    private final String name;
 }
