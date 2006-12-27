87b97a17d5173b15dc7e74883ded13ba1f755ab6
kou-tngt <kou-tngt@5bdb5694-003a-4164-b60f-8fcef0365520>
2006-12-27 04:58:09 +0000

    The method JavaBuildManager#getAvailableNameSpaceSet() which adds java default imported packages(java.lang) to result of the super method was added.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@239 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/java/JavaBuildManager.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/java/JavaBuildManager.java
@@ -1,6 +1,7 @@
 package jp.ac.osaka_u.ist.sel.metricstool.main.ast.java;
 
 import jp.ac.osaka_u.ist.sel.metricstool.main.ast.databuilder.DefaultBuildDataManager;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.AvailableNamespaceInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.AvailableNamespaceInfoSet;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.UnresolvedClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.UnresolvedReferenceTypeInfo;
@@ -14,6 +15,15 @@ public class JavaBuildManager extends DefaultBuildDataManager{
             classInfo.addSuperClass(OBJECT);
         }
     }
+    
+    @Override
+    public AvailableNamespaceInfoSet getAvailableNameSpaceSet() {
+        AvailableNamespaceInfoSet result = super.getAvailableNameSpaceSet();
+        result.add(JAVA_LANG);
+        return result;
+    }
+
+    private static final AvailableNamespaceInfo JAVA_LANG = new AvailableNamespaceInfo(new String[]{"java","lang"},true);
     
     private static final UnresolvedReferenceTypeInfo OBJECT = new UnresolvedReferenceTypeInfo(
             new AvailableNamespaceInfoSet(),new String[]{"java","lang","Object"});
