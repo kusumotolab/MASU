a4d53c860275b5cf4db51f1705c3dc5d6910fc5a
kou-tngt <kou-tngt@5bdb5694-003a-4164-b60f-8fcef0365520>
2007-01-18 11:11:50 +0000

    Fixed the bug that parameters of an instantiation of a java anonymous class are wrong.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@297 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/databuilder/expression/ExpressionBuilder.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/databuilder/expression/ExpressionBuilder.java
@@ -1,5 +1,6 @@
 package jp.ac.osaka_u.ist.sel.metricstool.main.ast.databuilder.expression;
 
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
@@ -10,74 +11,82 @@ import jp.ac.osaka_u.ist.sel.metricstool.main.ast.statemanager.StateChangeEvent;
 import jp.ac.osaka_u.ist.sel.metricstool.main.ast.token.AstToken;
 import jp.ac.osaka_u.ist.sel.metricstool.main.ast.visitor.AstVisitEvent;
 
-public abstract class ExpressionBuilder extends StateDrivenDataBuilder<ExpressionElement>{
 
-    public ExpressionBuilder(ExpressionElementManager expressionManager){
+public abstract class ExpressionBuilder extends StateDrivenDataBuilder<ExpressionElement> {
+
+    public ExpressionBuilder(ExpressionElementManager expressionManager) {
         this.expressionManager = expressionManager;
-        
+
         this.addStateManager(this.expressionStateManger);
     }
-    
+
     public final void entered(AstVisitEvent e) {
         super.entered(e);
-        
-        if (isActive() && isInExpression()){
-            if (isTriggerToken(e.getToken())){
-                expressionStackCountStack.push(expressionManager.getExpressionStackSize());
-            }
+
+        AstToken token = e.getToken();
+
+        if (isActive() && isInExpression() && isTriggerToken(token)
+                || isExpressionDelimiterToken(token)) {
+            expressionStackCountStack.push(expressionManager.getExpressionStackSize());
         }
     }
 
     public void exited(AstVisitEvent e) {
-        boolean isRelated = isActive() && isInExpression() && isTriggerToken(e.getToken());
-        if (isRelated){
+        AstToken token = e.getToken();
+
+        boolean isRelated = isActive() && isInExpression() && isTriggerToken(token);
+        if (isRelated || isExpressionDelimiterToken(token)) {
             assert (!expressionStackCountStack.isEmpty()) : "Illegal state: illegal stack size.";
-            
+
             int availableElementCount = expressionManager.getExpressionStackSize()
                     - expressionStackCountStack.pop();
-            
+
             assert (0 <= availableElementCount) : "Illegal state: illegal stack size.";
-            
+
             elementBuffer.clear();
-            
-            for(int i=0; i< availableElementCount; i++){
-                elementBuffer.add(0,expressionManager.popExpressionElement());
+
+            for (int i = 0; i < availableElementCount; i++) {
+                elementBuffer.add(0, expressionManager.popExpressionElement());
             }
         }
-        
+
         super.exited(e);
-        
-        if (isRelated){
+
+        if (isRelated) {
             afterExited(e);
         }
     }
-    
+
     @Override
     public void stateChangend(StateChangeEvent<AstVisitEvent> event) {
-        
+
     }
-    
+
     protected abstract void afterExited(AstVisitEvent event);
-    
-    protected boolean isInExpression(){
+
+    protected boolean isInExpression() {
         return expressionStateManger.inExpression();
     }
-    
+
     protected abstract boolean isTriggerToken(AstToken token);
-    
-    
-    protected final ExpressionElement[] getAvailableElements(){
+
+    protected final ExpressionElement[] getAvailableElements() {
         return elementBuffer.toArray(new ExpressionElement[elementBuffer.size()]);
     }
-    
-    protected final void pushElement(ExpressionElement element){
+
+    protected final void pushElement(ExpressionElement element) {
         expressionManager.pushExpressionElement(element);
     }
-    
+
+    private boolean isExpressionDelimiterToken(AstToken token) {
+        return token.isBlock();
+    }
+
     private final ExpressionElementManager expressionManager;
+
     private final ExpressionStateManager expressionStateManger = new ExpressionStateManager();
-    
+
     private Stack<Integer> expressionStackCountStack = new Stack<Integer>();
-    
+
     private final List<ExpressionElement> elementBuffer = new LinkedList<ExpressionElement>();
 }
