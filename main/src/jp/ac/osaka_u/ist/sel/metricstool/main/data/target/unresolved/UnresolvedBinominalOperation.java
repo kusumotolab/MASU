3f6453f93d2d1df494ba90b8fc12dc610be6d70c
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2007-02-10 08:14:53 +0000

    This class represents a (unresolved) binominal operation.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@316 5bdb5694-003a-4164-b60f-8fcef0365520

---
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/UnresolvedBinominalOperation.java
@@ -0,0 +1,123 @@
+package jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved;
+
+
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.OPERATOR;
+
+
+/**
+ * 未解決二項演算を格納するためのクラス
+ * 
+ * @author y-higo
+ * 
+ */
+public class UnresolvedBinominalOperation implements UnresolvedTypeInfo{
+
+    /**
+     * 引数なしコンストラクタ
+     */
+    public UnresolvedBinominalOperation() {
+    }
+
+    /**
+     * 演算子と2つのオペランドを与えて初期化する
+     * 
+     * @param operator 演算子
+     * @param firstOperand 第一（未解決）オペランド
+     * @param secondOperand 第二（未解決）オペランド
+     */
+    public UnresolvedBinominalOperation(final OPERATOR operator,
+            final UnresolvedTypeInfo firstOperand, final UnresolvedTypeInfo secondOperand) {
+
+        if ((null == operator) || (null == firstOperand) || (null == secondOperand)) {
+            throw new NullPointerException();
+        }
+
+        this.operator = operator;
+        this.firstOperand = firstOperand;
+        this.secondOperand = secondOperand;
+    }
+    
+    /**
+     * このクラスの型名を返す
+     * 
+     * @return このクラスの型名
+     */
+    public String getTypeName() {
+        return "UnresolvedBinominalOperation";
+    }
+
+    /**
+     * 演算子を取得する
+     * 
+     * @return 演算子
+     */
+    public OPERATOR getOperator() {
+        return this.operator;
+    }
+
+    /**
+     * 第一（未解決）オペランドを取得する
+     * 
+     * @return 第一（未解決）オペランド
+     */
+    public UnresolvedTypeInfo getFirstOperand() {
+        return this.firstOperand;
+    }
+
+    /**
+     * 第二（未解決）オペランドを取得する
+     * 
+     * @return 第二（未解決）オペランド
+     */
+    public UnresolvedTypeInfo getSecondOperand() {
+        return this.secondOperand;
+    }
+
+    /**
+     * 演算子をセットする
+     * 
+     * @param operator 演算子
+     */
+    public void setOperator(final OPERATOR operator) {
+
+        if (null == operator) {
+            throw new NullPointerException();
+        }
+
+        this.operator = operator;
+    }
+
+    /**
+     * 第一（未解決）オペランドをセットする
+     * 
+     * @param firstOperand 第一（未解決）オペランド
+     */
+    public void setFirstOperand(final UnresolvedTypeInfo firstOperand) {
+
+        if (null == operator) {
+            throw new NullPointerException();
+        }
+
+        this.firstOperand = firstOperand;
+    }
+
+    /**
+     * 第二（未解決）オペランドをセットする
+     * 
+     * @param secondOperand 第二（未解決）オペランド
+     */
+    public void setSecondOperand(final UnresolvedTypeInfo secondOperand) {
+
+        if (null == operator) {
+            throw new NullPointerException();
+        }
+
+        this.secondOperand = secondOperand;
+    }
+
+    private OPERATOR operator;
+
+    private UnresolvedTypeInfo firstOperand;
+
+    private UnresolvedTypeInfo secondOperand;
+}
