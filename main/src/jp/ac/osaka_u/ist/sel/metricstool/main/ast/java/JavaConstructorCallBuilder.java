27241add522361be86c9a4d669f576c0966f1941
kou-tngt <kou-tngt@5bdb5694-003a-4164-b60f-8fcef0365520>
2007-02-17 19:05:24 +0000

    Fixed the bug that when a reference name of first argment of a super class's constructor call is longer than the super class's reference name, ArrayIndexOutOfBoundsException was thrown. Gnaths PR Num 11 was fixed.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@345 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/java/JavaConstructorCallBuilder.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/java/JavaConstructorCallBuilder.java
@@ -1,5 +1,6 @@
 package jp.ac.osaka_u.ist.sel.metricstool.main.ast.java;
 
+
 import jp.ac.osaka_u.ist.sel.metricstool.main.ast.databuilder.BuildDataManager;
 import jp.ac.osaka_u.ist.sel.metricstool.main.ast.databuilder.expression.ConstructorCallBuilder;
 import jp.ac.osaka_u.ist.sel.metricstool.main.ast.databuilder.expression.ExpressionElement;
@@ -14,121 +15,130 @@ import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.UnresolvedM
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.UnresolvedReferenceTypeInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved.UnresolvedTypeInfo;
 
-public class JavaConstructorCallBuilder extends ConstructorCallBuilder{
 
-    public JavaConstructorCallBuilder(ExpressionElementManager expressionManager, BuildDataManager buildDataManager) {
-        super(expressionManager,buildDataManager);
+public class JavaConstructorCallBuilder extends ConstructorCallBuilder {
+
+    public JavaConstructorCallBuilder(ExpressionElementManager expressionManager,
+            BuildDataManager buildDataManager) {
+        super(expressionManager, buildDataManager);
         this.buildDataManager = buildDataManager;
     }
-    
+
     @Override
     protected void afterExited(AstVisitEvent event) {
         super.afterExited(event);
-        
+
         AstToken token = event.getToken();
-        if (token.equals(JavaAstToken.CONSTRUCTOR_CALL)){
+        if (token.equals(JavaAstToken.CONSTRUCTOR_CALL)) {
             buildInnerConstructorCall(buildDataManager.getCurrentClass());
-        } else if (token.equals(JavaAstToken.SUPER_CONSTRUCTOR_CALL)){
-            buildSuperConstructorCall(buildDataManager.getCurrentClass().getSuperClasses().iterator().next());
+        } else if (token.equals(JavaAstToken.SUPER_CONSTRUCTOR_CALL)) {
+            buildSuperConstructorCall(buildDataManager.getCurrentClass().getSuperClasses()
+                    .iterator().next());
         }
     }
-    
-    protected void buildNewConstructorCall(){
+
+    protected void buildNewConstructorCall() {
         ExpressionElement[] elements = getAvailableElements();
-        
-        assert(elements.length > 0) : "Illegal state: constructor element not found.";
-        
-        assert(elements[0] instanceof TypeElement) : "Illegal state: constructor owner is not type.";
-        
-        if (isJavaArrayInstantiation(elements)){
+
+        assert (elements.length > 0) : "Illegal state: constructor element not found.";
+
+        assert (elements[0] instanceof TypeElement) : "Illegal state: constructor owner is not type.";
+
+        if (isJavaArrayInstantiation(elements)) {
             //配列のnew文はこっちで処理する
-            UnresolvedTypeInfo type = (UnresolvedTypeInfo)elements[0].getType();
-            pushElement(TypeElement.getInstance(resolveArrayElement(type,elements)));
+            UnresolvedTypeInfo type = (UnresolvedTypeInfo) elements[0].getType();
+            pushElement(TypeElement.getInstance(resolveArrayElement(type, elements)));
         } else {
             //それ以外は普通に処理する
             super.buildNewConstructorCall();
         }
     }
-    
-    protected void buildInnerConstructorCall(UnresolvedClassInfo currentClass){
+
+    protected void buildInnerConstructorCall(UnresolvedClassInfo currentClass) {
         ExpressionElement[] elements = getAvailableElements();
         String className = currentClass.getClassName();
-        
-        UnresolvedMethodCall constructorCall = new UnresolvedMethodCall(currentClass,className,true);
+
+        UnresolvedMethodCall constructorCall = new UnresolvedMethodCall(currentClass, className,
+                true);
         resolveParameters(constructorCall, elements, 0);
         pushElement(new MethodCallElement(constructorCall));
         buildDataManager.addMethodCall(constructorCall);
     }
-    
-    protected void buildSuperConstructorCall(UnresolvedTypeInfo superClass){
+
+    protected void buildSuperConstructorCall(UnresolvedTypeInfo superClass) {
         ExpressionElement[] elements = getAvailableElements();
-        
-        UnresolvedReferenceTypeInfo superClassInfo = (UnresolvedReferenceTypeInfo)superClass;
-        
+
+        UnresolvedReferenceTypeInfo superClassInfo = (UnresolvedReferenceTypeInfo) superClass;
+
         int argStartIndex = 0;
-        
-        String[] referenceName = superClassInfo.getReferenceName();
-        String className = referenceName[referenceName.length-1];
-        
-        if (elements.length > 0 && elements[0] instanceof TypeElement){
+
+        String[] superClassReferenceName = superClassInfo.getReferenceName();
+        String className = superClassReferenceName[superClassReferenceName.length - 1];
+
+        if (elements.length > 0 && elements[0] instanceof TypeElement) {
             //苦し紛れの特別処理
             //elementsの1個目がUnresolvedReferenceTypeInfoでありかつsuperClassのアウタークラスであるなら
             //それはOuterClass.this.super()という呼び出し形式であるとみなす
-            
-            UnresolvedTypeInfo type = ((TypeElement)elements[0]).getType();
-            if (type instanceof UnresolvedReferenceTypeInfo){
-                String[] referenceName2 = ((UnresolvedReferenceTypeInfo)type).getReferenceName();
-                boolean match = true;
-                for(int i=0; i < referenceName2.length; i++){
-                    if (!referenceName2[i].equals(referenceName[i])){
-                        match = false;
-                        break;
+
+            UnresolvedTypeInfo type = ((TypeElement) elements[0]).getType();
+            if (type instanceof UnresolvedReferenceTypeInfo) {
+                String[] firstElementReference = ((UnresolvedReferenceTypeInfo) type)
+                        .getReferenceName();
+                if (firstElementReference.length < superClassReferenceName.length) {
+                    boolean match = true;
+                    for (int i = 0; i < firstElementReference.length; i++) {
+                        if (!firstElementReference[i].equals(superClassReferenceName[i])) {
+                            match = false;
+                            break;
+                        }
                     }
-                }
-                if (match){
-                    argStartIndex = 1;
+                    if (match) {
+                        argStartIndex = 1;
+                    }
                 }
             }
         }
-        
-        assert(null != className) : "Illegal state: unexpected ownerClass type.";
-        
-        UnresolvedMethodCall constructorCall = new UnresolvedMethodCall(superClass,className,true);
+
+        assert (null != className) : "Illegal state: unexpected ownerClass type.";
+
+        UnresolvedMethodCall constructorCall = new UnresolvedMethodCall(superClass, className, true);
         resolveParameters(constructorCall, elements, argStartIndex);
         pushElement(new MethodCallElement(constructorCall));
         buildDataManager.addMethodCall(constructorCall);
-        
+
     }
 
-    
-    protected boolean isJavaArrayInstantiation(ExpressionElement[] elements){
-        for(ExpressionElement element : elements){
-            if (element.equals(JavaArrayInstantiationElement.getInstance())){
+    protected boolean isJavaArrayInstantiation(ExpressionElement[] elements) {
+        for (ExpressionElement element : elements) {
+            if (element.equals(JavaArrayInstantiationElement.getInstance())) {
                 return true;
             }
         }
         return false;
     }
-    
-    protected UnresolvedArrayTypeInfo resolveArrayElement(UnresolvedTypeInfo type,ExpressionElement[] elements){
-        int i=1;
+
+    protected UnresolvedArrayTypeInfo resolveArrayElement(UnresolvedTypeInfo type,
+            ExpressionElement[] elements) {
+        int i = 1;
         int dimension = 0;
-        while(i < elements.length && elements[i].equals(JavaArrayInstantiationElement.getInstance())){
+        while (i < elements.length
+                && elements[i].equals(JavaArrayInstantiationElement.getInstance())) {
             dimension++;
             i++;
         }
-        
-        if (dimension > 0){
-            return UnresolvedArrayTypeInfo.getType(type,dimension);
+
+        if (dimension > 0) {
+            return UnresolvedArrayTypeInfo.getType(type, dimension);
         } else {
             return null;
         }
     }
-    
+
     @Override
     protected boolean isTriggerToken(AstToken token) {
-        return super.isTriggerToken(token) || token.equals(JavaAstToken.CONSTRUCTOR_CALL) || token.equals(JavaAstToken.SUPER_CONSTRUCTOR_CALL);
+        return super.isTriggerToken(token) || token.equals(JavaAstToken.CONSTRUCTOR_CALL)
+                || token.equals(JavaAstToken.SUPER_CONSTRUCTOR_CALL);
     }
-    
+
     private final BuildDataManager buildDataManager;
 }
