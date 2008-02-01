cd894d16b8842bbb9fd58eb990711fa972ec2b82
higo <higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2008-02-01 16:39:33 +0000

    Method "resolveEntityUsage" was implemented.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@516 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/UnresolvedConstructorCallInfo.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/UnresolvedConstructorCallInfo.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassInfoManager;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassTypeInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ConstructorCallInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.EntityUsageInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.FieldInfoManager;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.MethodInfoManager;
-import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassTypeInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ParameterInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TargetClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TargetMethodInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TypeInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.UnknownEntityUsageInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.external.ExternalClassInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.external.ExternalMethodInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.security.MetricsToolSecurityManager;
 
 
@@ -73,8 +77,67 @@ public final class UnresolvedConstructorCallInfo extends UnresolvedMemberCallInf
         final ClassInfo referencedClass = ((ClassTypeInfo) classType).getReferencedClass();
         if (referencedClass instanceof TargetClassInfo) {
 
+            // まずは利用可能なメソッドから検索
+            {
+                // 利用可能なメソッド一覧を取得
+                final List<TargetMethodInfo> availableMethods = NameResolver.getAvailableMethods(
+                        (TargetClassInfo) referencedClass, usingClass);
+
+                // 利用可能なメソッドから，未解決メソッドと一致するものを検索
+                // メソッド名，引数の型のリストを用いて，このメソッドの呼び出しであるかどうかを判定
+                for (final TargetMethodInfo availableMethod : availableMethods) {
+
+                    // 呼び出し可能なメソッドが見つかった場合
+                    if (availableMethod.canCalledWith(name, actualParameters)) {
+                        this.resolvedInfo = new ConstructorCallInfo(availableMethod);
+                        return this.resolvedInfo;
+                    }
+                }
+            }
+
+            // 利用可能なメソッドが見つからなかった場合は，外部クラスである親クラスがあるはず．
+            // そのクラスのメソッドを使用しているとみなす
+            {
+                final ExternalClassInfo externalSuperClass = NameResolver
+                        .getExternalSuperClass((TargetClassInfo) referencedClass);
+                if (null != externalSuperClass) {
+
+                    final ExternalMethodInfo methodInfo = new ExternalMethodInfo(this.getName(),
+                            externalSuperClass, true);
+                    final List<ParameterInfo> dummyParameters = NameResolver
+                            .createParameters(actualParameters);
+                    methodInfo.addParameters(dummyParameters);
+                    methodInfoManager.add(methodInfo);
+
+                    // 外部クラスに新規で外部メソッド変数（ExternalMethodInfo）を追加したので型は不明
+                    this.resolvedInfo = new ConstructorCallInfo(methodInfo);
+                    return this.resolvedInfo;
+                }
+
+                assert false : "Here shouldn't be reached!";
+            }
+
+            // 見つからなかった処理を行う
+            {
+                err.println("Can't resolve method Call : " + this.getName());
+
+                usingMethod.addUnresolvedUsage(this);
+
+                this.resolvedInfo = UnknownEntityUsageInfo.getInstance();
+                return this.resolvedInfo;
+            }
+
         } else if (referencedClass instanceof ExternalClassInfo) {
 
+            final ExternalMethodInfo methodInfo = new ExternalMethodInfo(this.getName(),
+                    referencedClass, true);
+            final List<ParameterInfo> parameters = NameResolver.createParameters(actualParameters);
+            methodInfo.addParameters(parameters);
+            methodInfoManager.add(methodInfo);
+
+            // 外部クラスに新規で外部メソッド(ExternalMethodInfo)を追加したので型は不明．
+            this.resolvedInfo = new ConstructorCallInfo(methodInfo);
+            return this.resolvedInfo;
         }
 
         // TODO 
