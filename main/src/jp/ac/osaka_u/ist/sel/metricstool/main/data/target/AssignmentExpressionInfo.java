76a4f7713f1f9d55ae84ef643523188e5126b51c
higo <higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2008-03-05 11:20:22 +0000

    Two interface and a class were added.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@622 5bdb5694-003a-4164-b60f-8fcef0365520

---
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/AssignmentExpressionInfo.java
@@ -0,0 +1,92 @@
+package jp.ac.osaka_u.ist.sel.metricstool.main.data.target;
+
+
+/**
+ * This class represents assignment-statement
+ * 
+ * @author higo
+ *
+ */
+public class AssignmentExpressionInfo {
+
+    public AssignmentExpressionInfo(final VariableUsageInfo leftVariable,
+            final ExpressionInfo rightExpression, final int fromLine, final int fromColumn,
+            final int toLine, final int toColumn) {
+
+        this.leftVariable = leftVariable;
+        this.rightExpression = rightExpression;
+
+        this.fromLine = fromLine;
+        this.fromColumn = fromColumn;
+        this.toLine = toLine;
+        this.toColumn = toColumn;
+    }
+    
+    public VariableUsageInfo getLeftVariable(){
+        return this.leftVariable;
+    }
+    
+    public ExpressionInfo getRightVariable(){
+        return this.rightExpression;
+    }
+
+    /**
+     * 開始行を返す
+     * 
+     * @return 開始行
+     */
+    public final int getFromLine() {
+        return this.fromLine;
+    }
+
+    /**
+     * 開始列を返す
+     * 
+     * @return 開始列
+     */
+    public final int getFromColumn() {
+        return this.fromColumn;
+    }
+
+    /**
+     * 終了行を返す
+     * 
+     * @return 終了行
+     */
+    public final int getToLine() {
+        return this.toLine;
+    }
+
+    /**
+     * 終了列を返す
+     * 
+     * @return 終了列
+     */
+    public final int getToColumn() {
+        return this.toColumn;
+    }
+
+    /**
+     * 開始行を保存するための変数
+     */
+    private final int fromLine;
+
+    /**
+     * 開始列を保存するための変数
+     */
+    private final int fromColumn;
+
+    /**
+     * 終了行を保存するための変数
+     */
+    private final int toLine;
+
+    /**
+     * 開始列を保存するための変数
+     */
+    private final int toColumn;
+
+    private final VariableUsageInfo leftVariable;
+
+    private final ExpressionInfo rightExpression;
+}
