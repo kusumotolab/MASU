b0b62cc85fb9a6326c5f88f13d1b0974acec23c1
higo <higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2008-05-12 15:54:19 +0000

    class "StaticInitializerInfo" was added.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@672 5bdb5694-003a-4164-b60f-8fcef0365520

---
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/StaticInitializerInfo.java
@@ -0,0 +1,25 @@
+package jp.ac.osaka_u.ist.sel.metricstool.main.data.target;
+
+
+/**
+ * スタティックイニシャライザを表すクラス
+ * 
+ * @author higo
+ *
+ */
+public final class StaticInitializerInfo extends LocalSpaceInfo {
+
+    /**
+     * 必要な情報を与えて，オブジェクトを初期化
+     * 
+     * @param ownerClass 所有クラス
+     * @param fromLine 開始行
+     * @param fromColumn 開始列
+     * @param toLine 終了行
+     * @param toColumn 終了列
+     */
+    public StaticInitializerInfo(final ClassInfo ownerClass, final int fromLine,
+            final int fromColumn, final int toLine, final int toColumn) {
+        super(ownerClass, fromLine, fromColumn, toLine, toColumn);
+    }
+}
--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/UnresolvedStaticInitializerInfo.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/UnresolvedStaticInitializerInfo.java
@@ -1,41 +1,79 @@
 package jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved;
 
+
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.CallableUnitInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassInfoManager;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.FieldInfoManager;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.MethodInfoManager;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.StaticInitializerInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TargetClassInfo;
-import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.UnitInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.security.MetricsToolSecurityManager;
+
 
 /**
  * 未解決 static initializer を表すクラス
  * 
- * @author t-miyake
+ * @author t-miyake, higo
  */
-public class UnresolvedStaticInitializerInfo extends UnresolvedLocalSpaceInfo {
+public class UnresolvedStaticInitializerInfo extends
+        UnresolvedLocalSpaceInfo<StaticInitializerInfo> {
 
+    /**
+     * 所有クラスを与えて，オブジェクトを初期化
+     * 
+     * @param ownerClass 所有クラス
+     */
     public UnresolvedStaticInitializerInfo(final UnresolvedClassInfo ownerClass) {
         super();
-        
-        if(null == ownerClass) {
+
+        if (null == ownerClass) {
             throw new IllegalArgumentException("ownerClass is null");
         }
-        
+
         this.ownerClass = ownerClass;
     }
 
+    /**
+     * 名前解決を行う
+     */
     @Override
-    public UnitInfo resolve(TargetClassInfo usingClass, CallableUnitInfo usingMethod,
-            ClassInfoManager classInfoManager, FieldInfoManager fieldInfoManager,
-            MethodInfoManager methodInfoManager) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+    public StaticInitializerInfo resolve(final TargetClassInfo usingClass,
+            final CallableUnitInfo usingMethod, final ClassInfoManager classInfoManager,
+            final FieldInfoManager fieldInfoManager, final MethodInfoManager methodInfoManager) {
+
+        // 不正な呼び出しでないかをチェック
+        MetricsToolSecurityManager.getInstance().checkAccess();
+        if ((null == usingClass) || (null == usingMethod) || (null == classInfoManager)
+                || (null == methodInfoManager)) {
+            throw new NullPointerException();
+        }
+
+        // 既に解決済みである場合は，キャッシュを返す
+        if (this.alreadyResolved()) {
+            return this.getResolved();
+        }
+
+        // 使用位置を取得
+        final int fromLine = this.getFromLine();
+        final int fromColumn = this.getFromColumn();
+        final int toLine = this.getToLine();
+        final int toColumn = this.getToColumn();
+
+        // 所有クラスを取得
+        final UnresolvedClassInfo unresolvedOwnerClass = this.getOwnerClass();
+        final ClassInfo ownerClass = unresolvedOwnerClass.resolve(usingClass, usingMethod,
+                classInfoManager, fieldInfoManager, methodInfoManager);
+
+        this.resolvedInfo = new StaticInitializerInfo(ownerClass, fromLine, fromColumn, toLine,
+                toColumn);
+        return this.resolvedInfo;
     }
-    
+
     public final UnresolvedClassInfo getOwnerClass() {
         return this.ownerClass;
     }
-    
+
     private final UnresolvedClassInfo ownerClass;
 
 }
