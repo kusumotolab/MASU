61c697b4e378f83dc7af0d89cfa310cb3ef003e4
higo <higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2008-05-12 13:53:35 +0000

    Javadoc comments were added.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@666 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/NotResolvedException.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/NotResolvedException.java
@@ -1,8 +1,12 @@
 package jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved;
 
 
-
-
+/**
+ * 名前解決されていない情報を利用しようとした時にスローされる例外
+ * 
+ * @author higo
+ *
+ */
 public class NotResolvedException extends RuntimeException {
 
     public NotResolvedException() {
