188673ff6776582ab32331b2ab9960804ea08bc7
higo <higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2008-12-17 09:23:19 +0000

    EmptyExpressionInfo was added. This class is used for "return ;", "for ( ; ; )" and so on.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@830 5bdb5694-003a-4164-b60f-8fcef0365520

---
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/EmptyExpressionInfo.java
@@ -0,0 +1,59 @@
+package jp.ac.osaka_u.ist.sel.metricstool.main.data.target;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * 空の式を表すクラス
+ * return ; や for ( ; ; ) などに用いる
+ * 
+ * @author higo
+ *
+ */
+public class EmptyExpressionInfo extends EntityUsageInfo implements ExpressionInfo {
+
+    /**
+    * @param fromLine 開始行
+    * @param fromColumn 開始列
+    * @param toLine 終了行
+    * @param toColumn 終了列
+    */
+    public EmptyExpressionInfo(final int fromLine, final int fromColumn, final int toLine,
+            final int toColumn) {
+        super(fromLine, fromColumn, toLine, toColumn);
+    }
+
+    /**
+     * void 型を返す
+     * 
+     * return void型 
+     */
+    @Override
+    public TypeInfo getType() {
+        return VoidTypeInfo.getInstance();
+    }
+
+    /**
+     * 長さ0のStringを返す
+     * 
+     * return 長さ0のString
+     */
+    @Override
+    public String getText() {
+        return "";
+    }
+
+    /**
+     * 使用されている変数のSetを返す．
+     * 実際はなにも使用されていないので，空のSetが返される．
+     * 
+     * @return 使用されている変数のSet
+     */
+    @Override
+    public Set<VariableUsageInfo<? extends VariableInfo<? extends UnitInfo>>> getVariableUsages() {
+        return new HashSet<VariableUsageInfo<?>>();
+    }
+
+}
