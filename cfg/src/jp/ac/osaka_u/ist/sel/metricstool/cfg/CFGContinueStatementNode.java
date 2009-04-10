b6ea38930085bbb4f7e61e0be1528cdec024bb8f
higo <higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2009-04-10 09:51:58 +0000

    *** empty log message ***
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1035 5bdb5694-003a-4164-b60f-8fcef0365520

--- cfg/src/jp/ac/osaka_u/ist/sel/metricstool/cfg/CFGContinueStatementNode.java
+++ cfg/src/jp/ac/osaka_u/ist/sel/metricstool/cfg/CFGContinueStatementNode.java
@@ -4,9 +4,20 @@ package jp.ac.osaka_u.ist.sel.metricstool.cfg;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ContinueStatementInfo;
 
 
+/**
+ * continue文を表すノードのためのクラス
+ * 
+ * @author higo
+ *
+ */
 public class CFGContinueStatementNode extends CFGJumpStatementNode {
 
-    CFGContinueStatementNode(final ContinueStatementInfo continueStatement) {
+    /**
+     * ノードを生成するcontinue文を与えて初期化
+     * 
+     * @param continueStatement
+     */
+    public CFGContinueStatementNode(final ContinueStatementInfo continueStatement) {
         super(continueStatement);
     }
 }
