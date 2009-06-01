16f75643616a3bda460d843819f0ebd05acac53c
higo <higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2009-06-01 12:29:18 +0000

    *** empty log message ***
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1087 5bdb5694-003a-4164-b60f-8fcef0365520

--- pdg/src/jp/ac/osaka_u/ist/sel/metricstool/pdg/ParameterDeclarationStatementInfo.java
+++ pdg/src/jp/ac/osaka_u/ist/sel/metricstool/pdg/ParameterDeclarationStatementInfo.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.CallInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassTypeInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ParameterInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.SingleStatementInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TypeInfo;
@@ -67,6 +68,11 @@ public class ParameterDeclarationStatementInfo extends SingleStatementInfo {
     @Override
     public Set<VariableUsageInfo<? extends VariableInfo<? extends UnitInfo>>> getVariableUsages() {
         return VariableUsageInfo.EmptySet;
+    }
+
+    @Override
+    public Set<ClassTypeInfo> getThrownExceptions() {
+        return Collections.unmodifiableSet(new HashSet<ClassTypeInfo>());
     }
 
     public ParameterInfo getParameter() {
