964a9f7875fe906270bb4e516fa00c507c0a2532
g-yamada <g-yamada@5bdb5694-003a-4164-b60f-8fcef0365520>
2009-06-08 04:35:22 +0000

    *bug fixed  *asseartion error with AbstractList.java
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1101 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/statemanager/innerblock/SimpleBlockStateManager.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/statemanager/innerblock/SimpleBlockStateManager.java
@@ -11,7 +11,7 @@ public class SimpleBlockStateManager extends InnerBlockStateManager {
     protected boolean isDefinitionEvent(final AstVisitEvent event) {
         AstToken parentToken = event.getParentToken();
         return  event.getToken().isBlock() && !parentToken.isBlockDefinition()
-                && !parentToken.isSynchronized();
+                && !parentToken.isSynchronized() && !parentToken.isInstantiation();
     }
 
 }
