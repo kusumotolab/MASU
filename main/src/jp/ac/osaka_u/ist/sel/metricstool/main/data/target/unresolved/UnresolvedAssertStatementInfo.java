0653ff6efbabfd6095618cefa96c984d4daa4387
a-saitoh <a-saitoh@5bdb5694-003a-4164-b60f-8fcef0365520>
2009-07-27 15:30:11 +0000

    Fixed a bug that gets wrong location of EmptyExpressionInfo in assert-statement
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1161 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/UnresolvedAssertStatementInfo.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/UnresolvedAssertStatementInfo.java
@@ -66,7 +66,7 @@ public class UnresolvedAssertStatementInfo extends
 
         final UnresolvedExpressionInfo<?> unresolvedMessageExpression = this.getMessageExpression();
         final ExpressionInfo messageExpression = null == unresolvedMessageExpression ? new EmptyExpressionInfo(
-                usingMethod, toLine, toLine, toColumn, toColumn)
+                usingMethod, toLine, toColumn, toLine, toColumn)
                 : unresolvedMessageExpression.resolve(usingClass, usingMethod, classInfoManager,
                         fieldInfoManager, methodInfoManager);
 
