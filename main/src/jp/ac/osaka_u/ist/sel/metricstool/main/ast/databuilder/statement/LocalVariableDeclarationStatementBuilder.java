2f711a78f3608cdaf122d86dc899f11f243e5164
g-yamada <g-yamada@5bdb5694-003a-4164-b60f-8fcef0365520>
2009-11-01 16:38:34 +0000

    * Emergency fix (Bug id 2890330)
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1212 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/databuilder/statement/LocalVariableDeclarationStatementBuilder.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/ast/databuilder/statement/LocalVariableDeclarationStatementBuilder.java
@@ -54,10 +54,13 @@ public class LocalVariableDeclarationStatementBuilder extends
             final int fromLine, final int fromColumn, final int toLine, final int toColumn) {
         final UnresolvedVariableDeclarationStatementInfo declarationStatement = new UnresolvedVariableDeclarationStatementInfo(
                 declarationUsage, initializerExpression);
+        // FIXME: this is temporal patch. fix ANTLR grammar file
+        final int correctToLine = declarationUsage.getToLine();
+        final int correctToColumn = declarationUsage.getToColumn();
         declarationStatement.setFromLine(fromLine);
         declarationStatement.setFromColumn(fromColumn);
-        declarationStatement.setToLine(toLine);
-        declarationStatement.setToColumn(toColumn);
+        declarationStatement.setToLine(correctToLine);
+        declarationStatement.setToColumn(correctToColumn);
 
         final UnresolvedLocalSpaceInfo<? extends LocalSpaceInfo> currentLocal = this.buildDataManager
                 .getCurrentLocalSpace();
