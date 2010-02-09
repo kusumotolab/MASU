1db1cff3ab38bf7c403b18b4ae869b81d2d226c2
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2010-02-09 16:53:08 +0000

    Bug that incorrectly count the same classes.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1256 5bdb5694-003a-4164-b60f-8fcef0365520

--- cbo/src/jp/ac/osaka_u/ist/sel/metricstool/cbo/CBOPlugin.java
+++ cbo/src/jp/ac/osaka_u/ist/sel/metricstool/cbo/CBOPlugin.java
@@ -1,8 +1,11 @@
 package jp.ac.osaka_u.ist.sel.metricstool.cbo;
 
-import java.util.HashSet;
-import java.util.Set;
 
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassTypeInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.LocalVariableInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ParameterInfo;
@@ -14,6 +17,7 @@ import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.UnitInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.VariableInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.plugin.AbstractClassMetricPlugin;
 
+
 /**
  * CBO(CKメトリクスの1つ)を計算するメトリクス
  * <p>
@@ -24,98 +28,111 @@ import jp.ac.osaka_u.ist.sel.metricstool.main.plugin.AbstractClassMetricPlugin;
  */
 public class CBOPlugin extends AbstractClassMetricPlugin {
 
-	/**
-	 * 引数で与えられたクラスのCBOを計算する
-	 * 
-	 * @param targetClass
-	 *            CBO計算対象クラス
-	 * @return 計算結果
-	 */
-	@Override
-	protected Number measureClassMetric(TargetClassInfo targetClass) {
+    /**
+     * 引数で与えられたクラスのCBOを計算する
+     * 
+     * @param targetClass
+     *            CBO計算対象クラス
+     * @return 計算結果
+     */
+    @Override
+    protected Number measureClassMetric(TargetClassInfo targetClass) {
 
-		Set<ClassTypeInfo> classes = new HashSet<ClassTypeInfo>();
+        SortedSet<ClassInfo<?, ?, ?, ?>> classes = new TreeSet<ClassInfo<?, ?, ?, ?>>();
 
-		// フィールドで使用されているクラス型を取得
-		for (final TargetFieldInfo field : targetClass.getDefinedFields()) {
-			final TypeInfo type = field.getType();
-			if (type instanceof ClassTypeInfo) {
-				classes.add((ClassTypeInfo) type);
-			}
-		}
+        // フィールドで使用されているクラス型を取得
+        for (final TargetFieldInfo field : targetClass.getDefinedFields()) {
+            final TypeInfo type = field.getType();
+            classes.addAll(this.getCohesiveClasses(type));
+        }
 
-		// メソッドで使用されているクラス型を取得
-		for (final TargetMethodInfo method : targetClass.getDefinedMethods()) {
+        // メソッドで使用されているクラス型を取得
+        for (final TargetMethodInfo method : targetClass.getDefinedMethods()) {
 
-			// 返り値についての処理
-			{
-				final TypeInfo returnType = method.getReturnType();
-				if (returnType instanceof ClassTypeInfo) {
-					classes.add((ClassTypeInfo) returnType);
-				}
-			}
+            // 返り値についての処理
+            {
+                final TypeInfo returnType = method.getReturnType();
+                classes.addAll(this.getCohesiveClasses(returnType));
+            }
 
-			// 引数のついての処理
-			for (final ParameterInfo parameter : method.getParameters()) {
-				final TypeInfo parameterType = parameter.getType();
-				if (parameterType instanceof ClassTypeInfo) {
-					classes.add((ClassTypeInfo) parameterType);
-				}
-			}
+            // 引数のついての処理
+            for (final ParameterInfo parameter : method.getParameters()) {
+                final TypeInfo parameterType = parameter.getType();
+                classes.addAll(this.getCohesiveClasses(parameterType));
+            }
 
-			// ローカル変数についての処理
-			for (final VariableInfo<? extends UnitInfo> variable : LocalVariableInfo
-					.getLocalVariables(method.getDefinedVariables())) {
-				final TypeInfo variableType = variable.getType();
-				if (variableType instanceof ClassTypeInfo) {
-					classes.add((ClassTypeInfo) variableType);
-				}
-			}
-		}
+            // ローカル変数についての処理
+            for (final VariableInfo<? extends UnitInfo> variable : LocalVariableInfo
+                    .getLocalVariables(method.getDefinedVariables())) {
+                final TypeInfo variableType = variable.getType();
+                classes.addAll(this.getCohesiveClasses(variableType));
+            }
+        }
 
-		// 自分自身は取り除く
-		classes.remove(targetClass);
+        // 自分自身は取り除く
+        classes.remove(targetClass);
 
-		return classes.size();
-	}
+        return classes.size();
+    }
 
-	/**
-	 * このプラグインの簡易説明を１行で返す
-	 * 
-	 * @return 簡易説明文字列
-	 */
-	@Override
-	protected String getDescription() {
-		return "Measuring the CBO metric.";
-	}
+    /**
+     * 与えられた型で利用されているクラスのSortedSetを返す
+     * 
+     * @param type 対象の型
+     * @return typeで利用されているクラスのSortedSet
+     */
+    private SortedSet<ClassInfo<?, ?, ?, ?>> getCohesiveClasses(final TypeInfo type) {
 
-	/**
-	 * このプラグインが計測するメトリクスの名前を返す
-	 * 
-	 * @return CBO
-	 */
-	@Override
-	protected String getMetricName() {
-		return "CBO";
-	}
+        final SortedSet<ClassInfo<?, ?, ?, ?>> cohesiveClasses = new TreeSet<ClassInfo<?, ?, ?, ?>>();
 
-	/**
-	 * このプラグインがフィールドに関する情報を利用するかどうかを返すメソッド． trueを返す．
-	 * 
-	 * @return true．
-	 */
-	@Override
-	protected boolean useFieldInfo() {
-		return true;
-	}
+        if (type instanceof ClassTypeInfo) {
+            final ClassTypeInfo classType = (ClassTypeInfo) type;
+            cohesiveClasses.add(classType.getReferencedClass());
+            for (final TypeInfo typeArgument : classType.getTypeArguments()) {
+                cohesiveClasses.addAll(this.getCohesiveClasses(typeArgument));
+            }
+        }
 
-	/**
-	 * このプラグインがメソッドに関する情報を利用するかどうかを返すメソッド． trueを返す．
-	 * 
-	 * @return true．
-	 */
-	@Override
-	protected boolean useMethodInfo() {
-		return true;
-	}
+        return Collections.unmodifiableSortedSet(cohesiveClasses);
+    }
+
+    /**
+     * このプラグインの簡易説明を１行で返す
+     * 
+     * @return 簡易説明文字列
+     */
+    @Override
+    protected String getDescription() {
+        return "Measuring the CBO metric.";
+    }
+
+    /**
+     * このプラグインが計測するメトリクスの名前を返す
+     * 
+     * @return CBO
+     */
+    @Override
+    protected String getMetricName() {
+        return "CBO";
+    }
+
+    /**
+     * このプラグインがフィールドに関する情報を利用するかどうかを返すメソッド． trueを返す．
+     * 
+     * @return true．
+     */
+    @Override
+    protected boolean useFieldInfo() {
+        return true;
+    }
+
+    /**
+     * このプラグインがメソッドに関する情報を利用するかどうかを返すメソッド． trueを返す．
+     * 
+     * @return true．
+     */
+    @Override
+    protected boolean useMethodInfo() {
+        return true;
+    }
 }
