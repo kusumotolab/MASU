a7fd4e287ae6702787149bb38516f719a20f9f92
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2010-06-28 14:12:59 +0000

    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1366 5bdb5694-003a-4164-b60f-8fcef0365520

--- pdg/src/jp/ac/osaka_u/ist/sel/metricstool/pdg/node/PDGMethodEnterNode.java
+++ pdg/src/jp/ac/osaka_u/ist/sel/metricstool/pdg/node/PDGMethodEnterNode.java
@@ -7,10 +7,10 @@ import java.util.Set;
 
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.CallInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.CallableUnitInfo;
-import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ClassTypeInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ConditionInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.LocalSpaceInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.Position;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ReferenceTypeInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.UnitInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.VariableInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.VariableUsageInfo;
@@ -70,8 +70,8 @@ public class PDGMethodEnterNode extends PDGControlNode {
 		}
 
 		@Override
-		public Set<ClassTypeInfo> getThrownExceptions() {
-			return new HashSet<ClassTypeInfo>();
+		public Set<ReferenceTypeInfo> getThrownExceptions() {
+			return new HashSet<ReferenceTypeInfo>();
 		}
 
 		@Override
