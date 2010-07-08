86d3c58a0ad75812fdd10972f7ad9a5cb2bc7061
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2010-07-08 15:58:24 +0000

    Method "getAvailableMethods" was refactored.
    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1392 5bdb5694-003a-4164-b60f-8fcef0365520

--- main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/NameResolver.java
+++ main/src/jp/ac/osaka_u/ist/sel/metricstool/main/data/target/unresolved/NameResolver.java
@@ -1,6 +1,7 @@
 package jp.ac.osaka_u.ist.sel.metricstool.main.data.target.unresolved;
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -17,6 +18,7 @@ import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.ExternalClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.FieldInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.InnerClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.MethodInfo;
+import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TargetInnerClassInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TypeParameterInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.TypeParameterizable;
 
@@ -69,7 +71,7 @@ public final class NameResolver {
         if (null == innerClass) {
             throw new IllegalArgumentException();
         }
-        
+
         final ClassInfo outerClass = innerClass.getOuterClass();
         return outerClass instanceof InnerClassInfo ? NameResolver
                 .getOuterstClass((InnerClassInfo) outerClass) : outerClass;
@@ -422,204 +424,6 @@ public final class NameResolver {
     }
 
     /**
-     * 「現在のクラス」で利用可能なメソッド一覧を返す．
-     * ここで，「利用可能なメソッド」とは，「現在のクラス」で定義されているメソッド，及びその親クラスで定義されているメソッドのうち子クラスからアクセスが可能なメソッドである．
-     * 利用可能なメソッドは List に格納されている． リストの先頭から優先順位の高いメソッド（つまり，クラス階層において下位のクラスに定義されているメソッド）が格納されている．
-     * 
-     * @param thisClass 現在のクラス
-     * @return 利用可能なメソッド一覧
-     */
-    private static List<MethodInfo> getAvailableMethods(final ClassInfo currentClass) {
-
-        if (null == currentClass) {
-            throw new IllegalArgumentException();
-        }
-
-        // チェックしたクラスを入れるためのキャッシュ，キャッシュにあるクラスは二度目はフィールド取得しない（ループ構造対策）
-        final Set<ClassInfo> checkedClasses = new HashSet<ClassInfo>();
-
-        // 利用可能な変数を代入するためのリスト
-        final List<MethodInfo> availableMethods = new LinkedList<MethodInfo>();
-
-        // 最も外側のクラスを取得
-        final ClassInfo outestClass;
-        if (currentClass instanceof InnerClassInfo) {
-            outestClass = NameResolver.getOuterstClass((InnerClassInfo) currentClass);
-
-            // 自クラスで定義されたメソッドを追加
-            availableMethods.addAll(currentClass.getDefinedMethods());
-            checkedClasses.add(currentClass);
-
-            // 内部クラスで定義されたメソッドを追加
-            for (final InnerClassInfo innerClass : currentClass.getInnerClasses()) {
-                final List<MethodInfo> availableMethodsDefinedInInnerClasses = NameResolver
-                        .getAvailableMethodsDefinedInInnerClasses((ClassInfo) innerClass,
-                                checkedClasses);
-                availableMethods.addAll(availableMethodsDefinedInInnerClasses);
-            }
-
-            // 親クラスで定義されたメソッドを追加
-            for (final ClassInfo superClass : ClassTypeInfo.convert(currentClass.getSuperClasses())) {
-                final List<MethodInfo> availableMethodsDefinedInSuperClasses = NameResolver
-                        .getAvailableMethodsDefinedInSuperClasses(outestClass, superClass,
-                                checkedClasses);
-                availableMethods.addAll(availableMethodsDefinedInSuperClasses);
-            }
-
-        } else {
-            outestClass = currentClass;
-        }
-
-        // 最も外側のクラスで定義されたメソッドを追加
-        availableMethods.addAll(outestClass.getDefinedMethods());
-        checkedClasses.add(outestClass);
-
-        // 内部クラスで定義されたメソッドを追加
-        for (final InnerClassInfo innerClass : outestClass.getInnerClasses()) {
-            final List<MethodInfo> availableMethodsDefinedInInnerClasses = NameResolver
-                    .getAvailableMethodsDefinedInInnerClasses((ClassInfo) innerClass,
-                            checkedClasses);
-            availableMethods.addAll(availableMethodsDefinedInInnerClasses);
-        }
-
-        // 親クラスで定義されたメソッドを追加
-        for (final ClassInfo superClass : ClassTypeInfo.convert(outestClass.getSuperClasses())) {
-            final List<MethodInfo> availableMethodsDefinedInSuperClasses = NameResolver
-                    .getAvailableMethodsDefinedInSuperClasses(outestClass, superClass,
-                            checkedClasses);
-            availableMethods.addAll(availableMethodsDefinedInSuperClasses);
-        }
-
-        return Collections.unmodifiableList(availableMethods);
-    }
-
-    /**
-     * 引数で与えられたクラスとその内部クラスで定義されたメソッドのうち，外側のクラスで利用可能なメソッドの List を返す
-     * 
-     * @param innerClassInfo クラス
-     * @param checkedClasses 既にチェックしたクラスのキャッシュ
-     * @return 外側のクラスで利用可能なメソッドの List
-     */
-    private static List<MethodInfo> getAvailableMethodsDefinedInInnerClasses(
-            final ClassInfo innerClassInfo, final Set<ClassInfo> checkedClasses) {
-
-        if ((null == innerClassInfo) || (null == checkedClasses)) {
-            throw new IllegalArgumentException();
-        }
-
-        // 既にチェックしたクラスである場合は何もせずに終了する
-        if (checkedClasses.contains(innerClassInfo)) {
-            return new LinkedList<MethodInfo>();
-        }
-
-        // 無名クラスであれば何もせずに終了する
-        if (innerClassInfo instanceof AnonymousClassInfo) {
-            return new LinkedList<MethodInfo>();
-        }
-
-        final List<MethodInfo> availableMethods = new LinkedList<MethodInfo>();
-
-        final ClassInfo classInfo = (ClassInfo) innerClassInfo;
-
-        availableMethods.addAll(classInfo.getDefinedMethods());
-        checkedClasses.add(classInfo);
-
-        // 内部クラスで定義されたメソッドを追加
-        for (final InnerClassInfo innerClass : classInfo.getInnerClasses()) {
-            final List<MethodInfo> availableMethodsDefinedInInnerClasses = NameResolver
-                    .getAvailableMethodsDefinedInInnerClasses((ClassInfo) innerClass,
-                            checkedClasses);
-            availableMethods.addAll(availableMethodsDefinedInInnerClasses);
-        }
-
-        // 親クラスで定義されたメソッドを追加
-        for (final ClassInfo superClass : ClassTypeInfo.convert(classInfo.getSuperClasses())) {
-            final List<MethodInfo> availableMethodsDefinedInSuperClasses = NameResolver
-                    .getAvailableMethodsDefinedInSuperClasses(classInfo, superClass, checkedClasses);
-            availableMethods.addAll(availableMethodsDefinedInSuperClasses);
-        }
-
-        return Collections.unmodifiableList(availableMethods);
-    }
-
-    /**
-     * 引数で与えられたクラスとその親クラスで定義されたメソッドのうち，子クラスで利用可能なメソッドの List を返す
-     * 
-     * @param classInfo クラス
-     * @param checkedClasses 既にチェックしたクラスのキャッシュ
-     * @return 子クラスで利用可能なメソッドの List
-     */
-    private static List<MethodInfo> getAvailableMethodsDefinedInSuperClasses(
-            final ClassInfo subClass, final ClassInfo superClass,
-            final Set<ClassInfo> checkedClasses) {
-
-        if ((null == subClass) || (null == superClass) || (null == checkedClasses)) {
-            throw new IllegalArgumentException();
-        }
-
-        // 既にチェックしたクラスである場合は何もせずに終了する
-        if (checkedClasses.contains(superClass)) {
-            return new LinkedList<MethodInfo>();
-        }
-
-        final List<MethodInfo> availableMethods = new LinkedList<MethodInfo>();
-
-        // 自クラスで定義されており，クラス階層可視性を持つメソッドを追加
-        for (final MethodInfo definedMethod : superClass.getDefinedMethods()) {
-
-            // 子クラスと親クラスの名前空間が同じ場合は，名前空間可視もしくは継承可視があればよい
-            if (subClass.getNamespace().equals(superClass.getNamespace())) {
-
-                if (definedMethod.isInheritanceVisible() || definedMethod.isNamespaceVisible()) {
-                    availableMethods.add(definedMethod);
-                }
-
-                // 子クラスと親クラスの名前空間が異なる場合は，継承可視があればよい
-            } else {
-
-                if (definedMethod.isInheritanceVisible()) {
-                    availableMethods.add(definedMethod);
-                }
-            }
-        }
-        checkedClasses.add(superClass);
-
-        // 内部クラスで定義されたメソッドを追加
-        for (final InnerClassInfo innerClass : superClass.getInnerClasses()) {
-            final List<MethodInfo> availableMethodsDefinedInInnerClasses = NameResolver
-                    .getAvailableMethodsDefinedInInnerClasses((ClassInfo) innerClass,
-                            checkedClasses);
-            for (final MethodInfo method : availableMethodsDefinedInInnerClasses) {
-
-                // 子クラスと親クラスの名前空間が同じ場合は，名前空間可視もしくは継承可視があればよい
-                if (subClass.getNamespace().equals(superClass.getNamespace())) {
-
-                    if (method.isInheritanceVisible() || method.isNamespaceVisible()) {
-                        availableMethods.add(method);
-                    }
-
-                    // 子クラスと親クラスの名前空間が異なる場合は，継承可視があればよい
-                } else {
-
-                    if (method.isInheritanceVisible()) {
-                        availableMethods.add(method);
-                    }
-                }
-            }
-        }
-
-        // 親クラスで定義されたメソッドを追加
-        for (final ClassInfo superSuperClass : ClassTypeInfo.convert(superClass.getSuperClasses())) {
-            final List<MethodInfo> availableMethodsDefinedInSuperClasses = NameResolver
-                    .getAvailableMethodsDefinedInSuperClasses(subClass, superSuperClass,
-                            checkedClasses);
-            availableMethods.addAll(availableMethodsDefinedInSuperClasses);
-        }
-
-        return Collections.unmodifiableList(availableMethods);
-    }
-
-    /**
      * 「使用されるクラス」が「使用するクラス」において使用される場合に，利用可能なフィールド一覧を返す．
      * ここで，「利用可能なフィールド」とは，「使用されるクラス」で定義されているフィールド，及びその親クラスで定義されているフィールドのうち子クラスからアクセスが可能なフィールドである．
      * また，「使用されるクラス」と「使用するクラス」の名前空間を比較し，より正確に利用可能なフィールドを取得する． 子クラスで利用可能なフィールド一覧は List に格納されている．
@@ -685,75 +489,6 @@ public final class NameResolver {
             }
 
             return Collections.unmodifiableList(availableFields);
-        }
-    }
-
-    /**
-     * 「使用されるクラス」が「使用するクラス」において使用される場合に，利用可能なメソッド一覧を返す．
-     * ここで，「利用可能なメソッド」とは，「使用されるクラス」で定義されているメソッド，及びその親クラスで定義されているメソッドのうち子クラスからアクセスが可能なメソッドである．
-     * また，「使用されるクラス」と「使用するクラス」の名前空間を比較し，より正確に利用可能なメソッドを取得する． 子クラスで利用可能なメソッド一覧は List に格納されている．
-     * リストの先頭から優先順位の高いメソッド（つまり，クラス階層において下位のクラスに定義されているメソッド）が格納されている．
-     * 
-     * @param usedClass 使用されるクラス
-     * @param usingClass 使用するクラス
-     * @return 利用可能なメソッド一覧
-     */
-    public static List<MethodInfo> getAvailableMethods(final ClassInfo usedClass,
-            final ClassInfo usingClass) {
-
-        if ((null == usedClass) || (null == usingClass)) {
-            throw new IllegalArgumentException();
-        }
-
-        // 使用されるクラスの最も外側のクラスを取得
-        final ClassInfo usedOutestClass;
-        if (usedClass instanceof InnerClassInfo) {
-            usedOutestClass = NameResolver.getOuterstClass((InnerClassInfo) usedClass);
-        } else {
-            usedOutestClass = usedClass;
-        }
-
-        // 使用するクラスの最も外側のクラスを取得
-        final ClassInfo usingOutestClass;
-        if (usingClass instanceof InnerClassInfo) {
-            usingOutestClass = NameResolver.getOuterstClass((InnerClassInfo) usingClass);
-        } else {
-            usingOutestClass = usingClass;
-        }
-
-        // このクラスで定義されているメソッドのうち，使用するクラスで利用可能なメソッドを取得する
-        // 2つのクラスが同じ場合，全てのメソッドが利用可能
-        if (usedOutestClass.equals(usingOutestClass)) {
-
-            return NameResolver.getAvailableMethods(usedClass);
-
-            // 2つのクラスが同じ名前空間を持っている場合
-        } else if (usedOutestClass.getNamespace().equals(usingOutestClass.getNamespace())) {
-
-            final List<MethodInfo> availableMethods = new LinkedList<MethodInfo>();
-
-            // 名前空間可視性を持ったメソッドのみが利用可能
-            for (final MethodInfo method : NameResolver.getAvailableMethods(usedClass)) {
-                if (method.isNamespaceVisible()) {
-                    availableMethods.add(method);
-                }
-            }
-
-            return Collections.unmodifiableList(availableMethods);
-
-            // 違う名前空間を持っている場合
-        } else {
-
-            final List<MethodInfo> availableMethods = new LinkedList<MethodInfo>();
-
-            // 全可視性を持つメソッドのみが利用可能
-            for (final MethodInfo method : NameResolver.getAvailableMethods(usedClass)) {
-                if (method.isPublicVisible()) {
-                    availableMethods.add(method);
-                }
-            }
-
-            return Collections.unmodifiableList(availableMethods);
         }
     }
 
@@ -824,5 +559,102 @@ public final class NameResolver {
         }
 
         return Collections.unmodifiableList(typeParameters);
+    }
+
+    public static List<MethodInfo> getAvailableMethods(final ClassInfo usedClass,
+            final ClassInfo usingClass) {
+        return Collections.unmodifiableList(getAvailableMethods(usedClass, usingClass,
+                new HashSet<ClassInfo>()));
+    }
+
+    private static List<MethodInfo> getAvailableMethods(final ClassInfo usedClass,
+            final ClassInfo usingClass, final Set<ClassInfo> checkedClasses) {
+
+        // すでにチェックしているクラスであれば何もせずに抜ける
+        if (checkedClasses.contains(usedClass)) {
+            return Collections.<MethodInfo> emptyList();
+        }
+
+        // チェック済みクラスに追加
+        checkedClasses.add(usedClass);
+
+        // usedに定義されているメソッドのうち，利用可能なものを追加
+        final List<MethodInfo> availableMethods = new ArrayList<MethodInfo>();
+        availableMethods.addAll(extractAvailableMethods(usedClass, usingClass));
+
+        // usedの外クラスをチェック
+        if (usedClass instanceof InnerClassInfo) {
+            final ClassInfo outerClass = ((InnerClassInfo) usedClass).getOuterClass();
+            availableMethods.addAll(getAvailableMethods(outerClass, usingClass, checkedClasses));
+        }
+
+        // 親クラスをチェック
+        for (final ClassTypeInfo superClassType : usedClass.getSuperClasses()) {
+            final ClassInfo superClass = superClassType.getReferencedClass();
+            availableMethods.addAll(getAvailableMethods(superClass, usingClass, checkedClasses));
+        }
+
+        return availableMethods;
+    }
+
+    private static List<MethodInfo> extractAvailableMethods(final ClassInfo usedClass,
+            final ClassInfo usingClass) {
+
+        final List<MethodInfo> availableMethods = new ArrayList<MethodInfo>();
+
+        // usingとusedが等しい場合は，すべてのメソッドを使用可能
+        if (usingClass.equals(usedClass)) {
+            availableMethods.addAll(usedClass.getDefinedMethods());
+        }
+
+        // usingがusedのインナークラスであればすべてのメソッドを使用可能
+        if (usingClass.isInnerClass(usedClass)) {
+            availableMethods.addAll(usedClass.getDefinedMethods());
+        }
+
+        // usedがusingのインナークラスの場合は，すべてのメソッドを利用可能
+        if (usedClass.isInnerClass(usingClass)) {
+            availableMethods.addAll(usedClass.getDefinedMethods());
+        }
+
+        // usingがusedと同じパッケージであれば，private 以外のメソッドが使用可能
+        if (usingClass.getNamespace().equals(usedClass.getNamespace())) {
+            for (final MethodInfo method : usedClass.getDefinedMethods()) {
+                if (!method.isPrivateVisible()) {
+                    availableMethods.add(method);
+                }
+            }
+        }
+
+        // usingがインナークラスであれば，その最外クラスと，usedクラスが同じパッケージであれば使用可能
+        if (usingClass instanceof InnerClassInfo) {
+            final ClassInfo outestClass = TargetInnerClassInfo
+                    .getOutestClass((InnerClassInfo) usingClass);
+            if (outestClass.getNamespace().equals(usedClass.getNamespace())) {
+                for (final MethodInfo method : usedClass.getDefinedMethods()) {
+                    if (!method.isPrivateVisible()) {
+                        availableMethods.add(method);
+                    }
+                }
+            }
+        }
+
+        // usingがusedのサブクラスであれば,protected以外のメソッドが使用可能
+        if (usingClass.isSubClass(usedClass)) {
+            for (final MethodInfo method : usedClass.getDefinedMethods()) {
+                if (method.isInheritanceVisible()) {
+                    availableMethods.add(method);
+                }
+            }
+        }
+
+        // usingがusedと関係のないクラスであれば，publicのメソッドが利用可能
+        for (final MethodInfo method : usedClass.getDefinedMethods()) {
+            if (method.isPublicVisible()) {
+                availableMethods.add(method);
+            }
+        }
+
+        return availableMethods;
     }
 }
