010a27dfa59afb0d467919a05ca3c1b106facc27
y-higo <y-higo@5bdb5694-003a-4164-b60f-8fcef0365520>
2010-07-15 16:23:36 +0000

    git-svn-id: https://masu.svn.sourceforge.net/svnroot/masu/trunk@1419 5bdb5694-003a-4164-b60f-8fcef0365520

--- scdetector/src/jp/ac/osaka_u/ist/sdl/scdetector/PDGBuildingThread.java
+++ scdetector/src/jp/ac/osaka_u/ist/sdl/scdetector/PDGBuildingThread.java
@@ -1,5 +1,6 @@
 package jp.ac.osaka_u.ist.sdl.scdetector;
 
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 import jp.ac.osaka_u.ist.sdl.scdetector.gui.data.PDGController;
@@ -8,67 +9,65 @@ import jp.ac.osaka_u.ist.sel.metricstool.main.data.target.CallableUnitInfo;
 import jp.ac.osaka_u.ist.sel.metricstool.pdg.IPDGNodeFactory;
 import jp.ac.osaka_u.ist.sel.metricstool.pdg.IntraProceduralPDG;
 
+
 public class PDGBuildingThread<T extends CallableUnitInfo> implements Runnable {
 
-	public PDGBuildingThread(final T[] methods, final AtomicInteger index,
-			final IPDGNodeFactory pdgNodeFactory, final boolean data,
-			final boolean control, final boolean execution,
-			final boolean countObjectStateChange, final int dataDistance,
-			final int controlDistance, final int executionDistance) {
+    public PDGBuildingThread(final T[] methods, final AtomicInteger index,
+            final IPDGNodeFactory pdgNodeFactory, final boolean data, final boolean control,
+            final boolean execution, final boolean countObjectStateChange, final int dataDistance,
+            final int controlDistance, final int executionDistance) {
 
-		if (null == methods || null == index || null == pdgNodeFactory) {
-			throw new IllegalArgumentException();
-		}
+        if (null == methods || null == index || null == pdgNodeFactory) {
+            throw new IllegalArgumentException();
+        }
 
-		this.pdgNodeFactory = pdgNodeFactory;
-		this.methods = methods;
-		this.index = index;
-		this.data = data;
-		this.control = control;
-		this.execution = execution;
-		this.countObjectStateChange = countObjectStateChange;
-		this.dataDistance = dataDistance;
-		this.controlDistance = controlDistance;
-		this.executionDistance = executionDistance;
-	}
+        this.pdgNodeFactory = pdgNodeFactory;
+        this.methods = methods;
+        this.index = index;
+        this.data = data;
+        this.control = control;
+        this.execution = execution;
+        this.countObjectStateChange = countObjectStateChange;
+        this.dataDistance = dataDistance;
+        this.controlDistance = controlDistance;
+        this.executionDistance = executionDistance;
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		while (true) {
+        while (true) {
 
-			final int index = this.index.getAndIncrement();
-			if (!(index < this.methods.length)) {
-				break;
-			}
+            final int index = this.index.getAndIncrement();
+            if (!(index < this.methods.length)) {
+                break;
+            }
 
-			final IntraProceduralPDG pdg = new IntraProceduralPDG(
-					this.methods[index], this.pdgNodeFactory,
-					new DefaultCFGNodeFactory(), this.data, this.control,
-					this.execution, this.countObjectStateChange,
-					this.dataDistance, this.controlDistance,
-					this.executionDistance);
-			PDGController.SINGLETON.put(this.methods[index], pdg);
-		}
-	}
+            final IntraProceduralPDG pdg = new IntraProceduralPDG(this.methods[index],
+                    this.pdgNodeFactory, new DefaultCFGNodeFactory(), this.data, this.control,
+                    this.execution, this.countObjectStateChange, true, false, this.dataDistance,
+                    this.controlDistance, this.executionDistance);
+            PDGController.SINGLETON.put(this.methods[index], pdg);
+        }
+    }
 
-	private final T[] methods;
+    private final T[] methods;
 
-	private final AtomicInteger index;
+    private final AtomicInteger index;
 
-	private final IPDGNodeFactory pdgNodeFactory;
+    private final IPDGNodeFactory pdgNodeFactory;
 
-	private final boolean data;
+    private final boolean data;
 
-	private final boolean control;
+    private final boolean control;
 
-	private final boolean execution;
+    private final boolean execution;
 
-	private final boolean countObjectStateChange;
+    private final boolean countObjectStateChange;
 
-	private final int dataDistance;
+    private final int dataDistance;
 
-	private final int controlDistance;
+    private final int controlDistance;
 
-	private final int executionDistance;
+    private final int executionDistance;
 }
