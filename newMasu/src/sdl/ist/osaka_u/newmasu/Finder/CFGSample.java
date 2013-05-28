e7c34866652eb97508438be112f3e05951f9d873
Shuhei Kimura <s-kimura@ist.osaka-u.ac.jp>
2013-05-28 17:07:40 +0900

    Refactor CFGNode

--- newMasu/src/sdl/ist/osaka_u/newmasu/Finder/CFGSample.java
+++ newMasu/src/sdl/ist/osaka_u/newmasu/Finder/CFGSample.java
@@ -6,7 +6,6 @@ import java.util.*;
 
 public class CFGSample extends ASTVisitor {
 
-    private Map<ASTNode, String> nameMap = new HashMap<>();
     private Integer _name = 0;
     private String getName(){
         return "NODE_" + _name.toString();
@@ -15,29 +14,11 @@ public class CFGSample extends ASTVisitor {
         _name++;
         return getName();
     }
-    private void nameMapping(ASTNode node){
-        nameMap.put(node, nextName());
-    }
-
-    @Override public boolean visit(CompilationUnit node){
-        TestWriter.newFile();
-        TestWriter.println("digraph CFG {");
-        return true;
-    }
-    @Override public void endVisit(CompilationUnit node){
-//        TestWriter.println("}");
-
-//        TestWriter.newFile();
-//        TestWriter.println("digraph CFG {");
-//        root.showChildren();
-        TestWriter.println("}");
-    }
 
     private static class CFGNode{
         private String label="";
         private String name="";
         private String shape="";
-        private String edge="";
 
         private boolean dummy=false;
 
@@ -50,7 +31,6 @@ public class CFGSample extends ASTVisitor {
                 this.label = cn.label;
                 this.name = cn.name;
                 this.shape = cn.shape;
-                this.edge = cn.edge;
                 this.dummy = false;
                 children = new ArrayList<>();
                 node = this;
@@ -69,22 +49,12 @@ public class CFGSample extends ASTVisitor {
             this.dummy = true;
         }
         public CFGNode(String label, String name){
-            this(label, name, "box", "");
+            this(label, name, "box");
         }
         public CFGNode(String label, String name, String shape){
-            this(label, name, shape, "");
-        }
-        public CFGNode(String label, String name, String shape, String edge){
             this.label = label.trim();
             this.name = name.trim();
             this.shape = shape;
-            this.edge = edge;
-        }
-
-        private String sanitize(String str){
-            str = str.trim();
-            str = str.replaceAll("\\\"", "\\\\\\\"");
-            return str;
         }
 
         private static Set<CFGNode> used;
@@ -92,7 +62,6 @@ public class CFGSample extends ASTVisitor {
             used = new HashSet<>();
             _showChildren();
         }
-
         private void _showChildren(){
             if(!used.contains(this)){
                 used.add(this);
@@ -103,40 +72,42 @@ public class CFGSample extends ASTVisitor {
                     if(edgeLabel.containsKey(Pair.of(this, cn)))
                         edge = edgeLabel.get(Pair.of(this, cn));
                     TestWriter.println(name + " -> " + cn.name + "[label=\"" + edge + "\"];");
-                    System.out.println(label + " -> " + cn.label);
                     cn._showChildren();
                 }
 
             }
         }
 
-
-        private static Map<Pair<CFGNode,CFGNode>, String> edgeLabel = new HashMap<>();
-        private static String trigger = null;
-        private static CFGNode triggerNode1 = null, triggerNode2 = null;
-        public void setTrigger(CFGNode n1, CFGNode n2, String str){
-            trigger = str;
-            triggerNode1 = n1;
-            triggerNode2 = n2;
-        }
-        private void addEdgeLabel(CFGNode node){
-            if(triggerNode1==null && triggerNode2!=null)
-                edgeLabel.put(Pair.of(node,triggerNode2),trigger);
-            else if(triggerNode1!=null && triggerNode2==null)
-                edgeLabel.put(Pair.of(triggerNode1,node),trigger);
-//        else
-//            edgeLabel.put(Pair.of(n1,n2),str);
-
-            trigger = null;
-            triggerNode1 = null;
-            triggerNode2 = null;
+        private String sanitize(String str){
+            str = str.trim();
+            str = str.replaceAll("\\\"", "\\\\\\\"");
+            return str;
         }
     }
 
+
+    private static Map<Pair<CFGNode,CFGNode>, String> edgeLabel = new HashMap<>();
+    private static String trigger = null;
+    private static CFGNode triggerNode1 = null, triggerNode2 = null;
+    public void setTrigger(CFGNode n1, CFGNode n2, String str){
+        trigger = str;
+        triggerNode1 = n1;
+        triggerNode2 = n2;
+    }
+    private static void addEdgeLabel(CFGNode node){
+        if(triggerNode1==null && triggerNode2!=null)
+            edgeLabel.put(Pair.of(node,triggerNode2),trigger);
+        else if(triggerNode1!=null && triggerNode2==null)
+            edgeLabel.put(Pair.of(triggerNode1,node),trigger);
+
+        trigger = null;
+        triggerNode1 = null;
+        triggerNode2 = null;
+    }
+
+
     private CFGNode root = new CFGNode("root","R");
     private CFGNode nowNode = root;
-
-
 
     //////////////////////////////////////////////////////////////////////////////////
 
@@ -144,7 +115,6 @@ public class CFGSample extends ASTVisitor {
         CFGNode cn = new CFGNode(node.toString(), nextName());
         nowNode = nowNode.addChild(cn);
     }
-
 
     @Override
     public boolean visit(IfStatement node){
@@ -154,13 +124,13 @@ public class CFGSample extends ASTVisitor {
         CFGNode dummy = new CFGNode(true);
 
         nowNode = cif;
-        nowNode.setTrigger(cif, null, "then");
+        setTrigger(cif, null, "then");
         DefaultProcessor.get(node.getThenStatement(), this).process(node.getThenStatement());
         nowNode.addChild(dummy);
 
         if(node.getElseStatement()!=null){
             nowNode = cif;
-            nowNode.setTrigger(cif, null, "else");
+            setTrigger(cif, null, "else");
             DefaultProcessor.get(node.getElseStatement(), this).process(node.getElseStatement());
             nowNode.addChild(dummy);
         }
@@ -177,12 +147,12 @@ public class CFGSample extends ASTVisitor {
         nowNode = nowNode.addChild(cond);
 
         nowNode = cond;
-        nowNode.setTrigger(cond, null, "then");
+        setTrigger(cond, null, "then");
         DefaultProcessor.get(node.getBody(), this).process(node.getBody());
         nowNode.addChild(cond);
 
         nowNode = cond;
-        nowNode.setTrigger(cond, null, "else");
+        setTrigger(cond, null, "else");
 
         return false;
     }
@@ -195,6 +165,15 @@ public class CFGSample extends ASTVisitor {
         return false;
     }
 
+    @Override public boolean visit(CompilationUnit node){
+        TestWriter.newFile();
+        TestWriter.println("digraph CFG {");
+        return true;
+    }
+    @Override public void endVisit(CompilationUnit node){
+        TestWriter.println("}");
+    }
+
     @Override
     public boolean visit(MethodDeclaration node){
         root = new CFGNode(node.getName().toString(),nextName(), "Msquare");
@@ -205,17 +184,5 @@ public class CFGSample extends ASTVisitor {
     public void endVisit(MethodDeclaration node){
         nowNode.addChild(new CFGNode("End " + node.getName().toString(), nextName(), "Msquare"));
         root.showChildren();
-//        TestWriter.println("}");
     }
-
-    /*
-    private String indent = "";
-    private void addIndent(){
-        indent += "  ";
-    }
-    private void delIndent(){
-        if(indent.length() > 2)
-            indent = indent.substring(0,indent.length()-2);
-    }
-    */
 }
