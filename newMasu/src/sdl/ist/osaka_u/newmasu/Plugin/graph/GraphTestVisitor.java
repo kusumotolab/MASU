341a59b49fe8951bec71f73eb0784b26dce6c3e7
Shuhei Kimura <s-kimura@ist.osaka-u.ac.jp>
2013-08-26 15:21:25 +0900

    refactor graph module

--- newMasu/src/sdl/ist/osaka_u/newmasu/Plugin/graph/GraphTestVisitor.java
+++ newMasu/src/sdl/ist/osaka_u/newmasu/Plugin/graph/GraphTestVisitor.java
@@ -8,9 +8,9 @@ import java.util.*;
 
 public class GraphTestVisitor extends ASTVisitor{
 
-    private Branch nowBranch = null;
-    private Set<Branch> allBranch = new LinkedHashSet<>();
     private Map<Pair<Node,Node>, String> edge = new LinkedHashMap<>();
+    private Node root = null;
+    private Node nowNode = null;
 
     // nodes - possible to be jumped
     private Stack<Node> enter = new Stack<>();
@@ -18,45 +18,34 @@ public class GraphTestVisitor extends ASTVisitor{
 
     public void def(ASTNode node){
         final Node n = new Node(node.toString(),false,"ellipse");
-        nowBranch.insert(n);
+        nowNode = nowNode.addChildren(n);
     }
 
     @Override
     public boolean visit(IfStatement node){
 
-      nowBranch.insert(new Node("{", true, "ellipse"));
+        nowNode = nowNode.addChildren(new Node("{", true, "ellipse"));
 
         final Node condNode = new Node(node.getExpression().toString(), false, "diamond");
-        nowBranch.insert(condNode);
+        nowNode = nowNode.addChildren(condNode);
 
         final Node dummy = new Node("}", true, "ellipse");
-        final Branch previousBranch = nowBranch;
-        //allBranch.add(previousBranch);
 
         // create then branch
-        final Branch thenBranch = previousBranch.newBranch();
-        allBranch.add(thenBranch);
-        nowBranch = thenBranch;
         GraphTestProcessor.get(node.getThenStatement(), this).process(node.getThenStatement());
-        nowBranch.insert(dummy);
+        nowNode.addChildren(dummy);
         // create edge label
-        final Pair<Node,Node> thenEdge = new Pair<>(thenBranch.getPath().get(0), thenBranch.getPath().get(1));
+        final Pair<Node,Node> thenEdge = new Pair<>(condNode, condNode.getChildren().get(0));
         edge.put(thenEdge, "true");
 
         // create else branch
-        final Branch elseBranch = previousBranch.newBranch();
-        allBranch.add(elseBranch);
-        nowBranch = elseBranch;
+        nowNode = condNode;
         if( node.getElseStatement() != null )
             GraphTestProcessor.get(node.getElseStatement(), this).process(node.getElseStatement());
-        nowBranch.insert(dummy);
+        nowNode = nowNode.addChildren(dummy);
         // create edge label
-        final Pair<Node,Node> elseEdge = new Pair<>(elseBranch.getPath().get(0), elseBranch.getPath().get(1));
+        final Pair<Node,Node> elseEdge = new Pair<>(condNode, condNode.getChildren().get(1));
         edge.put(elseEdge, "false");
-
-        nowBranch = new Branch();
-        allBranch.add(nowBranch);
-        nowBranch.insert(dummy);
 
         return false;
     }
@@ -64,44 +53,33 @@ public class GraphTestVisitor extends ASTVisitor{
     @Override
     public boolean visit(ForStatement node){
 
-        nowBranch.insert(new Node("{", true, "ellipse"));
+        nowNode =  nowNode.addChildren(new Node("{", true, "ellipse"));
+        nowNode = nowNode.addChildren(new Node(node.initializers().toString(), false, "ellipse"));
 
-        nowBranch.insert(new Node(node.initializers().toString(), false, "ellipse"));
+        final Node condNode = new Node(node.getExpression().toString(), false, "diamond");
+        nowNode = nowNode.addChildren(condNode);
 
-        Node condNode = new Node(node.getExpression().toString(), false, "diamond");
-        nowBranch.insert(condNode);
-
-        //final Node dummy = new Node("dummy", true, "ellipse");
         final Node dummy = new Node("}", true, "ellipse");
-        final Branch previousBranch = nowBranch;
-        //allBranch.add(previousBranch);
 
         // set enter & exit node
         enter.push(condNode);
         exit.push(dummy);
 
         // create then branch
-        final Branch thenBranch = previousBranch.newBranch();
-        allBranch.add(thenBranch);
-        nowBranch = thenBranch;
         GraphTestProcessor.get(node.getBody(), this).process(node.getBody());
-        nowBranch.insert(new Node(node.updaters().toString(), false, "ellipse"));
-        nowBranch.insert(condNode);
+        nowNode = nowNode.addChildren(new Node(node.updaters().toString(), false, "ellipse"));
+        nowNode = nowNode.addChildren(condNode);
         // create edge label
-        final Pair<Node,Node> thenEdge = new Pair<>(thenBranch.getPath().get(0), thenBranch.getPath().get(1));
+        final Pair<Node,Node> thenEdge = new Pair<>(
+                nowNode, nowNode.getChildren().get(0));
         edge.put(thenEdge, "true");
 
         // create else branch
-        final Branch elseBranch = previousBranch.newBranch();
-        allBranch.add(elseBranch);
-        elseBranch.insert(dummy);
+        condNode.addChildren(dummy);
+        nowNode = dummy;
         // create edge label
-        final Pair<Node,Node> elseEdge = new Pair<>(elseBranch.getPath().get(0), elseBranch.getPath().get(1));
+        final Pair<Node,Node> elseEdge = new Pair<>(condNode, dummy);
         edge.put(elseEdge, "false");
-
-        nowBranch = new Branch();
-        allBranch.add(nowBranch);
-        nowBranch.insert(dummy);
 
         // pop enter & exit node
         enter.pop();
@@ -111,42 +89,30 @@ public class GraphTestVisitor extends ASTVisitor{
     }
     @Override
     public boolean visit(WhileStatement node){
-
-        nowBranch.insert(new Node("{", true, "ellipse"));
-
+        nowNode =  nowNode.addChildren(new Node("{", true, "ellipse"));
         final Node condNode = new Node(node.getExpression().toString(), false, "diamond");
-        nowBranch.insert(condNode);
-
-        //final Node dummy = new Node("dummy", true, "ellipse");
+        nowNode = nowNode.addChildren(condNode);
         final Node dummy = new Node("}", true, "ellipse");
-        final Branch previousBranch = nowBranch;
-        //allBranch.add(previousBranch);
 
         // set enter & exit node
         enter.push(condNode);
         exit.push(dummy);
 
         // create then branch
-        final Branch thenBranch = previousBranch.newBranch();
-        allBranch.add(thenBranch);
-        nowBranch = thenBranch;
         GraphTestProcessor.get(node.getBody(), this).process(node.getBody());
-        nowBranch.insert(condNode);
+        nowNode = nowNode.addChildren(condNode);
         // create edge label
-        final Pair<Node,Node> thenEdge = new Pair<>(thenBranch.getPath().get(0), thenBranch.getPath().get(1));
+        final Pair<Node,Node> thenEdge = new Pair<>(
+                nowNode, nowNode.getChildren().get(0));
         edge.put(thenEdge, "true");
 
         // create else branch
-        final Branch elseBranch = previousBranch.newBranch();
-        allBranch.add(elseBranch);
-        elseBranch.insert(dummy);
+        condNode.addChildren(dummy);
+        nowNode = dummy;
         // create edge label
-        final Pair<Node,Node> elseEdge = new Pair<>(elseBranch.getPath().get(0), elseBranch.getPath().get(1));
+        final Pair<Node,Node> elseEdge = new Pair<>(
+                condNode, dummy);
         edge.put(elseEdge, "false");
-
-        nowBranch = new Branch();
-        allBranch.add(nowBranch);
-        nowBranch.insert(dummy);
 
         // pop enter & exit node
         enter.pop();
@@ -156,27 +122,20 @@ public class GraphTestVisitor extends ASTVisitor{
     }
     @Override
     public boolean visit(BreakStatement node){
-        nowBranch.insert(new Node(node.toString(), false, "triangle"));
-        nowBranch.insert(exit.peek());
-        nowBranch = new Branch();
-        allBranch.add(nowBranch);
+        nowNode = nowNode.addChildren(new Node(node.toString(), false, "triangle"));
+        nowNode.addChildren(exit.peek());
         return false;
     }
     @Override
     public boolean visit(ReturnStatement node){
-        nowBranch.insert(new Node(node.toString(), false, "triangle"));
-        nowBranch.insert(exit.get(0));
-//        nowBranch.getPath().getLast().setId(exit.get(0).getId());
-        nowBranch = new Branch();
-        allBranch.add(nowBranch);
+        nowNode = nowNode.addChildren(new Node(node.toString(), false, "triangle"));
+        nowNode.addChildren(exit.get(0));
         return false;
     }
     @Override
     public boolean visit(ContinueStatement node){
-        nowBranch.insert(new Node(node.toString(), false, "triangle"));
-        nowBranch.insert(enter.peek());
-        nowBranch = new Branch();
-        allBranch.add(nowBranch);
+        nowNode = nowNode.addChildren(new Node(node.toString(), false, "triangle"));
+        nowNode.addChildren(enter.peek());
         return false;
     }
 
@@ -188,7 +147,6 @@ public class GraphTestVisitor extends ASTVisitor{
             GraphTestProcessor.get(s, this).process(s);
         return false;
     }
-
 
 
     @Override public boolean visit(CompilationUnit node){
@@ -210,12 +168,10 @@ public class GraphTestVisitor extends ASTVisitor{
         }
         label += ")";
 
-        allBranch.clear();
-        nowBranch = new Branch();
-        allBranch.add(nowBranch);
-
         final Node start = new Node("Start " + label, false, "rect");
-        nowBranch.insert( start );
+        root = start;
+        nowNode = root;
+
         enter.push(start);
         exit.push(new Node("End " + label, false, "rect"));
 
@@ -223,83 +179,15 @@ public class GraphTestVisitor extends ASTVisitor{
     }
     @Override
     public void endVisit(MethodDeclaration node){
-/*
-        // exit(0) is references many places, so the ObjectID of exit(0) should not be changed.
-        // the id of exit(0) should be the id of the previous node.
-        final Node exit0 = exit.pop();
-        final Node last = nowBranch.getPath().getLast();
-        int id = exit0.getId();
-        if(last.getDummy())
-            id = last.getId();
-        exit0.setId(id);
-  */
-        nowBranch.insert(exit.pop());
-
-        // This may cause the duplication of the id of nodes, so self-loop in End should be removed.
-/*
-
-        for(Branch b : allBranch){
-            if(b.getPath().size()>=2 &&
-                    b.getPath().get(b.getPath().size()-2).getId()
-                            == b.getPath().getLast().getId()){
-                b.getPath().removeLast();
-            }
-        }
-
-*/
-        Node ent = enter.pop();
-        final Tree root = new Tree(ent);
-        Map<Node, Tree> node2tree = new LinkedHashMap<>();
-        node2tree.put(ent, root);
-
-        for(Branch b : allBranch){
-            for(Node n : b.getPath()){
-                if(!node2tree.containsKey(n))
-                    node2tree.put(n, new Tree(n));
-            }
-        }
-/*
-        for( Map.Entry<Node,Tree> e : node2tree.entrySet()){
-            System.out.println(e.getKey().getLabel());
-        }
-*/
-        for(Branch b : allBranch){
-            for(int i=0; i<b.getPath().size(); i++){
-                if(b.getPath().size()-i < 2)
-                    break;
-                Tree t1 = node2tree.get(b.getPath().get(i));
-                Tree t2 = node2tree.get(b.getPath().get(i+1));
-                t1.addChildren(t2);
-            }
-        }
+        nowNode.addChildren(exit.pop());
+        root = enter.pop();
 
         root.used.clear();
-//        root.dump();
-
-        Map<Pair<Tree,Tree>,String> treeEdge = new LinkedHashMap<>();
-        for(Map.Entry<Pair<Node,Node>,String> e : edge.entrySet()){
-            String s = e.getValue();
-            Pair<Tree,Tree> t = new Pair<>(node2tree.get(e.getKey().fst),
-                    node2tree.get(e.getKey().snd));
-            treeEdge.put(t,s);
-        }
-
-        root.used.clear();
-        root.removeDummy(treeEdge);
-        root.used.clear();
-        root.dump();
-
-//        for(Branch b : allBranch)
-//            b.removeEdge(edge);
+        root.removeDummy(edge);
 
         assert enter.isEmpty();
         assert exit.isEmpty();
 
-        root.used.clear();
-        root.print(treeEdge);
-/*
-        for(Branch b : allBranch)
-            b.print(edge);
-            */
+        root.print(edge);
     }
 }
--- newMasu/src/sdl/ist/osaka_u/newmasu/Plugin/graph/Node.java
+++ newMasu/src/sdl/ist/osaka_u/newmasu/Plugin/graph/Node.java
@@ -1,15 +1,29 @@
 package sdl.ist.osaka_u.newmasu.Plugin.graph;
 
+import com.sun.tools.javac.util.Pair;
+import sdl.ist.osaka_u.newmasu.Plugin.CFG.TestWriter;
+
+import java.util.*;
+
 public class Node {
 
     private String label = "";
     private Boolean isDummy = false;
     private String shape = "ellipse";
-    private int id;
     public void copy(Node node){
         label = node.label;
         isDummy = node.isDummy;
         shape = node.shape;
+    }
+
+    private List<Node> children = new LinkedList<>();
+
+    public List<Node> getChildren() {
+        return children;
+    }
+    public Node addChildren(Node node){
+        children.add(node);
+        return node;
     }
 
     public String getLabel() { return label; }
@@ -21,15 +35,8 @@ public class Node {
     public String getShape() { return shape; }
     public void setShape(String shape) { this.shape = shape; }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
     public Node(String name, Boolean dummy, String shape){
-        this();
         label=name; isDummy=dummy; this.shape=shape;
-    }
-    public Node(){
-        id = hashCode();
     }
 
     public String toGraph(){
@@ -40,6 +47,101 @@ public class Node {
         return label.trim();
     }
     public String toGraphId(){
-        return Integer.toString(id);
+        return Integer.toString(hashCode());
+    }
+
+
+
+
+    public static Set<Node> used = new LinkedHashSet<>();
+    public void removeDummy(Map<Pair<Node,Node>,String> edge){
+        used.clear();
+        __removeDummyWorker(edge);
+    }
+    private void __removeDummyWorker(Map<Pair<Node,Node>,String> edge){
+        if(used.contains(this))
+            return;
+        used.add(this);
+
+        // dummy node must have 0 or 1 children
+        while(!children.isEmpty()){
+            // if children contains dummy
+            boolean hasDummy = false;
+            for(Node c : children)
+                if(c.getDummy())
+                    hasDummy = true;
+            if(!hasDummy)
+                break;
+
+            Pair<Node,Node> del = null;
+            for(Node c1 : children){
+                for(Node c2 : c1.children){
+                    if(c1.getDummy()){
+                        del = new Pair<>(c1,c2);
+                        final Pair<Node,Node> e1 = new Pair<>(this, c1);
+                        final Pair<Node,Node> e2 = new Pair<>(c1, c2);
+                        final Pair<Node,Node> rep = new Pair<>(this, c2);
+                        if(edge.containsKey(e1)){
+                            final String s = edge.get(e1);
+                            edge.remove(e1);
+                            edge.put(rep,s);
+                        }
+                        if(edge.containsKey(e2)){
+                            final String s = edge.get(e2);
+                            edge.remove(e2);
+                            edge.put(rep,s);
+                        }
+                        break;
+                    }
+                }
+                if(del!=null)
+                    break;
+            }
+
+            if(del!=null){
+                children.remove(del.fst);
+                children.add(del.snd);
+            }
+        }
+
+        for(Node t : children)
+            t.__removeDummyWorker(edge);
+    }
+
+
+
+    private static Set<Node> usedNodeInToGraph = new LinkedHashSet<>();
+    public void print(Map<Pair<Node,Node>,String> edge){
+        usedNodeInToGraph.clear();
+        TestWriter.println(__toGraph(edge));
+    }
+    private String __toGraph(Map<Pair<Node,Node>,String> edge){
+        if(usedNodeInToGraph.contains(this))
+            return "";
+        usedNodeInToGraph.add(this);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(toGraph());
+        sb.append(System.lineSeparator());
+
+        for(Node c : children){
+            sb.append(toGraphId() + " -> " + c.toGraphId());
+
+            StringBuilder edgesb = new StringBuilder();
+            edgesb.append(" [label=\"");
+            Pair<Node,Node> p = new Pair<>(this,c);
+            if(edge.containsKey(p)){
+                edgesb.append(edge.get(p));
+            }
+            edgesb.append("\"];");
+            sb.append(edgesb.toString());
+
+            sb.append(System.lineSeparator());
+        }
+
+        for(Node f : children)
+            sb.append( f.__toGraph(edge) );
+
+        return sb.toString();
     }
 }
